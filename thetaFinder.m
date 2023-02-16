3d6a2506b635c2b45553c0fce5a782e57c0966a6
Frank McQuarrie <frankmac@uga.edu>
2023-02-16 10:56:24 -0500

    Create thetaFinder.m

---
+++ thetaFinder.m
@@ -0,0 +1,15 @@
+%FM 2/16/23
+% Trying an idea from advisors/Yargo: We have transceiver orientation as
+% compared to 0N rose, done, constant. For each hour timestep, we have the
+% angle at which the tides are flowing compared to 0N rose; comparing the
+% two will let us know when the tides are parallel or perpendicular to the
+% known transceiver angle (+/- a few degrees, TBD).
+
+
+%Idea: Tide is XX degrees, Transceivers are oriented forever at YY
+
+% thetaHourly = YY-XX
+% closeNuff = thetaHourly < 5 & thetaHourly >-5
+
+
+
