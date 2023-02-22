12515ac75234299f4adacc168109f740bac92889
Frank McQuarrie <frankmac@uga.edu>
2023-02-22 15:56:50 -0500

    Update thetaFinder.m

--- thetaFinder.m
+++ thetaFinder.m
@@ -66,70 +66,20 @@ end
 
 
 %%
-%FM: Trying to add my bowties to main panel figures
+%FM: Adding my bowties to main panel figures
 
-
-
-
-for COUNT = 1:2:length(AnglesR)
-    nameit= sprintf('Pairing %d Angle vs Tidal Ellipses',pairing(COUNT))
-    
-    figure()
-    set(gcf, 'Position',  [0, 0, 1000, 1000])
-
-    tiledlayout(2,2)
-    nexttile
-    polarscatter(AnglesR(1,COUNT),x(1),280,'X')
-    hold on
-    polarplot(AnglesR(1,COUNT:COUNT+1),x(1:2),'-.');
-    polarscatter(AnglesR(1,COUNT+1),x(1),200,'square','filled','k')
-    polarplot(tideAnglesR(1:2),x2(1:2),'r')
-    polarplot(tideAnglesR(3:4),x3(1:2),'r')
-    polarscatter(tideAnglesR(1:2),x2(1:2),130,'r','filled')
-    polarscatter(tideAnglesR(3:4),x3(1:2),'r','filled')
-    pax = gca;
-    pax.ThetaZeroLocation = 'top';
-    pax.ThetaDir = 'clockwise';
-    title(nameit)
-    
-    nexttile
-    plot(ut,vt)
-    hold on
-    scatter(ut(15393),vt(15393),'filled','r')
-    scatter(ut(15430),vt(15430),'filled','k')
-    title('OG')
-    xline(0)
-    yline(0)
-    axis equal
-
-    nexttile
-    plot(rotUtide(COUNT+1,:),rotVtide(COUNT+1,:))
-    hold on
-    scatter(rotUtide(COUNT+1,15393),rotVtide(COUNT+1,15393),'filled','r')
-    scatter(rotUtide(COUNT+1,15430),rotVtide(COUNT+1,15430),'filled','k')
-    xline(0)
-    yline(0)
-%     title(sprintf('Should be %0.1f CCW',rotatorsD(1,COUNT+1)))
-    title('X Axis: Square to X +')
-    axis equal
-    
-    nexttile
-    plot(rotUtide(COUNT,:),rotVtide(COUNT,:))
-    hold on
-    scatter(rotUtide(COUNT,15393),rotVtide(COUNT,15393),'filled','r')
-    scatter(rotUtide(COUNT,15430),rotVtide(COUNT,15430),'filled','k')
-%     plot(rotated1(COUNT,:),rotated2(COUNT,:),'k')
-%     plot(rotated3(COUNT,:),rotated4(COUNT,:),'k')
-%     scatter(rotated1(COUNT,:),rotated2(COUNT,:),'r','filled')
-%     scatter(rotated3(COUNT,:),rotated4(COUNT,:),'k','filled')
-    xline(0)
-    yline(0)
-%     title(sprintf('Should be %0.1f CCW',rotatorsD(1,COUNT)))
-    title('X Axis: X to Square +')
-    axis equal
-
-end
-
+% figure()
+% 
+% for COUNT = 1:length(parallelDT)
+%     nameit= sprintf('Pairing %d Parallel(R) and Perp.(G) to Tides',COUNT)
+%     nexttile
+%     plot(rotUtide(COUNT,:),rotVtide(COUNT,:))
+%     hold on
+%     scatter(parallelU{COUNT},parallelV{COUNT},'r','filled')
+%     scatter(perpendicularU{COUNT},perpendicularV{COUNT},'g','filled')
+%     axis equal
+%     title(nameit)
+% end
 
 
 
