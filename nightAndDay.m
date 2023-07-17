4ae82e559ac240ca8f0337827351f3ab1c352af5
Frank McQuarrie <frankmac@uga.edu>
2023-07-17 15:30:00 -0400

    work
    eork

--- nightAndDay.m
+++ nightAndDay.m
@@ -3,53 +3,62 @@
 
 %Run binnedAVG
 
+%Index formed by using the fullData area created in binnedAVG
+for COUNT= 1:length(fullData)
+    for k = 1:length(seasons)
+        daylightIndex{COUNT}{k}(1,:) =  fullData{COUNT}.sunlight==1 & fullData{COUNT}.season ==k;
+        daylightIndex{COUNT}{k}(2,:) =  fullData{COUNT}.sunlight==2 & fullData{COUNT}.season ==k;
+        daylightIndex{COUNT}{k}(3,:) =  fullData{COUNT}.sunlight==0 & fullData{COUNT}.season ==k;
+    
+        %Separating data by day, sunset, and night. "Sunset" is defined as
+        %the hour in which the sun sets, attempting to find crepuscular
+        %period.
+        dayHours{k,COUNT}    = fullData{1,COUNT}(daylightIndex{1,COUNT}{1,k}(1,:),:)
+        sunsetHours{k,COUNT}  = fullData{1,COUNT}(daylightIndex{1,COUNT}{1,k}(2,:),:)
+        nightHours{k,COUNT}  = fullData{1,COUNT}(daylightIndex{1,COUNT}{1,k}(3,:),:)
+        %Isolating just the detections and noise. All other variables can
+        %be helpful but this is just a way of finding averages 
+        dayDets{k}(COUNT,:)    = dayHours{k,COUNT}.detections;
+        nightDets{k}(COUNT,:)  = nightHours{k,COUNT}.detections;
+        sunsetDets{k}(COUNT,:)  = sunsetHours{k,COUNT}.detections;
+        daySounds{k}(COUNT,:)     =dayHours{k,COUNT}.noise;
+        nightSounds{k}(COUNT,:)   = nightHours{k,COUNT}.noise;
+        sunsetSounds{k}(COUNT,:)   = sunsetHours{k,COUNT}.noise;
 
-
-% clearvars -except fullData detections time bottom* receiverData fullTide*
-for COUNT = 1:length(fullData)
-    index{1,COUNT} = sunlight ==1;
-    index{2,COUNT} = sunlight ==0;
-    day{COUNT}    = fullData{COUNT}(index{1,COUNT},:)
-    night{COUNT}  = fullData{COUNT}(index{2,COUNT},:)
-    dayDets(COUNT,:)    = day{COUNT}.detections;
-    nightDets(COUNT,:)  = night{COUNT}.detections;
-    daySounds(COUNT,:)     =day{COUNT}.noise;
-    nightSounds(COUNT,:)   = night{COUNT}.noise;
+    end
 end
 
-noiseDay1    = mean(daySounds,1);
-noiseNight1  = mean(nightSounds,1);
-noiseDay    = mean(noiseDay1,'all')
-noiseNight  = mean(noiseNight1,'all')
-avgDay1    = mean(dayDets,1)
-avgNight1  = mean(nightDets,1)
-avgDay    = mean(avgDay1,'all')
-avgNight  = mean(avgNight1,'all')
 
-errDay    = std(avgDay1);
+for k = 1:length(seasons)
+    %1st row: DayHoursxSeasons
+    %2nd row: SunsetHoursxSeasons
+    %3rd row: NightHoursxSeasons
+    dayAverages(1,k)    = mean(daySounds{1,k}(:,:),'all')
+    dayAverages(2,k)  = mean(sunsetSounds{1,k}(:,:),'all')
+    dayAverages(3,k) = mean(nightSounds{1,k}(:,:),'all')
+    %Finding standard deviations/CIs of values
+    errDay(1,k)   = std(daySounds{1,k}(:))
+    errDay(2,k)   = std(sunsetSounds{1,k}(:))
+    errDay(3,k)   = std(nightSounds{1,k}(:))
+end
 
-errNight  = std(avgNight1);
 
-x = 1:length(dayDets(1,:));
-x1 = 1:length(nightDets(1,:));
-
-figure()
-hold on
-scatter(day{1}.time,avgDay1,'r','filled')
-scatter(night{1}.time,avgNight1,'b','filled')
 
 
 figure()
-scatter(1,avgDay,'r','filled')
+scatter(1:5,dayAverages(1,:),'r','filled')
 hold on
-scatter(3,avgNight,'k','filled')
-errorbar(1,avgDay,errDay,'LineStyle','none')
-errorbar(3,avgNight,errNight,'LineStyle','none')
-ylim([0 4])
-xlim([0.9 3.1])
-
-
-
+scatter(1:5,dayAverages(2,:),'o','filled')
+scatter(1:5,dayAverages(3,:),'k','filled')
+errorbar(1:5,dayAverages(1,:),errDay(1,:),'LineStyle','none')
+errorbar(1:5,dayAverages(2,:),errDay(2,:),'LineStyle','none')
+errorbar(1:5,dayAverages(3,:),errDay(3,:),'LineStyle','none')
+ylim([0480 800])
+xlim([0.85 5.2])
+xlabel('Seasons 1:5')
+ylabel('Ambient Sounds (db/mV)')
+title('Average across Entire Reef')
+legend('Day','Sunset','Night')
 %%
 %Frank separated all the bin creation to not clutter one massive script
 %with so many different goals.
