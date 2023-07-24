9b5454cf36005e1152598cab249f610d931bad03
Frank McQuarrie <frankmac@uga.edu>
2023-07-23 21:50:16 -0400

    Update windMagPlots.m

--- windMagPlots.m
+++ windMagPlots.m
@@ -9,117 +9,117 @@
 cd (localPlots)
 
 %Creating an X-axis to emulate the bins of Wind data, 0-14 m/s
-% X = 0:12;
-% 
-% seasonName = [{'Winter','Spring','Summer','Fall','Mariner''s Fall','Fall'}]
-% color = ['r','r','g','g','k','k','b','b','m','m'];
-% 
-% 
-% 
-% figure()
-% scatter(X,yearlyWindSpeed,'filled')
-% ylabel('Normalized Det. Efficiency')
-% xlabel('Wind Magnitude (m/s)')
-% title('Wind Magnitude''s Enabling of Detection Success')
-% 
-% 
-% figure()
-% yyaxis left
-% scatter(X,yearlyTiltVsWindSpeed,'b','filled')
-% ylabel('Avg Instrument Tilt')
-% xlabel('Wind Magnitude (m/s)')
-% 
-% yyaxis right
-% scatter(X,yearlyWindSpeed,'r','filled')
-% ylabel('Normalized Det. Efficiency')
-% 
-% title('Wind''s Effect on Moored Transceivers')
-% 
-% 
-% 
-% for COUNT = 1:length(normalizedWSpeed)
-%     figure()
-%     hold on
-%     for season = 1:length(seasons)
-%         scatter(X,normalizedWSpeed{COUNT}{season},'filled')
-% %         plot(X,normalizedWindSpeed{COUNT}{season})
-%     end
-%     nameit = sprintf('Wind Magnitude on Transceiver: %d',COUNT);
-%     title(nameit)
-%     ylabel('Normalized Det. Efficiency')
-%     xlabel('Wind Magnitude (m/s)')
-%    lgd =  legend('Winter','Spring','Summer','Fall','Mariners Fall',loc='upper left')
-%    lgd.Location = 'northwest';
-% %     exportgraphics(gcf,sprintf('Transceiver%dWinds.png',COUNT),'Resolution',300)
-% end
-% 
-% 
-% 
-% %All seasons together
-% for season = 1:length(seasons)
-%     figure()
-%     hold on
-%     for COUNT = 1:length(normalizedWSpeed)
-%         scatter(X,normalizedWSpeed{COUNT}{season},'filled')
-%     end
-%     nameit = sprintf('Winds During %s',seasonName{season});
-%     title(nameit)
-%     ylabel('Normalized Det. Efficiency')
-%     xlabel('Wind Magnitude (m/s)')
-% %     legend('Winter','Spring','Summer','Fall','Mariners Fall')
-% %     exportgraphics(gcf,sprintf('WindsDuring %s .png',seasonName{season}),'Resolution',300)
-% end
-% 
+X = 0:12;
+
+seasonName = [{'Winter','Spring','Summer','Fall','Mariner''s Fall','Fall'}]
+color = ['r','r','g','g','k','k','b','b','m','m'];
 
 
-%%
+
+figure()
+scatter(X,yearlyWindSpeed,'filled')
+ylabel('Normalized Det. Efficiency')
+xlabel('Wind Magnitude (m/s)')
+title('Wind Magnitude''s Enabling of Detection Success')
+
+
+figure()
+yyaxis left
+scatter(X,yearlyTiltVsWindSpeed,'b','filled')
+ylabel('Avg Instrument Tilt')
+xlabel('Wind Magnitude (m/s)')
+
+yyaxis right
+scatter(X,yearlyWindSpeed,'r','filled')
+ylabel('Normalized Det. Efficiency')
+
+title('Wind''s Effect on Moored Transceivers')
+
+
+
+for COUNT = 1:length(normalizedWSpeed)
+    figure()
+    hold on
+    for season = 1:length(seasons)
+        scatter(X,normalizedWSpeed{COUNT}{season},'filled')
+%         plot(X,normalizedWindSpeed{COUNT}{season})
+    end
+    nameit = sprintf('Wind Magnitude on Transceiver: %d',COUNT);
+    title(nameit)
+    ylabel('Normalized Det. Efficiency')
+    xlabel('Wind Magnitude (m/s)')
+   lgd =  legend('Winter','Spring','Summer','Fall','Mariners Fall',loc='upper left')
+   lgd.Location = 'northwest';
+%     exportgraphics(gcf,sprintf('Transceiver%dWinds.png',COUNT),'Resolution',300)
+end
+
+
+
+%All seasons together
+for season = 1:length(seasons)
+    figure()
+    hold on
+    for COUNT = 1:length(normalizedWSpeed)
+        scatter(X,normalizedWSpeed{COUNT}{season},'filled')
+    end
+    nameit = sprintf('Winds During %s',seasonName{season});
+    title(nameit)
+    ylabel('Normalized Det. Efficiency')
+    xlabel('Wind Magnitude (m/s)')
+%     legend('Winter','Spring','Summer','Fall','Mariners Fall')
+%     exportgraphics(gcf,sprintf('WindsDuring %s .png',seasonName{season}),'Resolution',300)
+end
+
+
+
+%
  
-% X = 0:2:22;
-% 
-% 
-% cd (localPlots)
-% 
-% seasonName = [{'Winter','Spring','Summer','Fall','Mariner''s Fall','Fall'}]
-% color = ['r','r','g','g','k','k','b','b','m','m'];
-% 
-% figure()
-% scatter(X,yearlyTiltVsWindSpeed,'filled')
-% xlabel('Instrument Tilt')
-% ylabel('Normalized Det. Efficiency')
-% title('Yearly Avg Det Efficiency w/ different tilts')
-% 
-% for COUNT = 1:length(normalizedTilt)
-%     figure()
-%     hold on
-%     for season = 1:length(seasons)
-%         scatter(X,normalizedTilt{COUNT}{season},'filled')
-%     end
+X = 0:2:22;
+
+
+cd (localPlots)
+
+seasonName = [{'Winter','Spring','Summer','Fall','Mariner''s Fall','Fall'}]
+color = ['r','r','g','g','k','k','b','b','m','m'];
+
+figure()
+scatter(X,yearlyTiltVsWindSpeed,'filled')
+xlabel('Instrument Tilt')
+ylabel('Normalized Det. Efficiency')
+title('Yearly Avg Det Efficiency w/ different tilts')
+
+for COUNT = 1:length(normalizedTilt)
+    figure()
+    hold on
+    for season = 1:length(seasons)
+        scatter(X,normalizedTilt{COUNT}{season},'filled')
+    end
+    nameit = sprintf('Tilt on Transceiver: %d',COUNT);
+    title(nameit)
+    ylabel('Normalized Det. Efficiency')
+    xlabel('Instrument Tilt')
+   lgd =  legend('Winter','Spring','Summer','Fall','Mariners Fall',loc='upper left')
+%    lgd.Location = 'northwest';
+%     exportgraphics(gcf,sprintf('Transceiver%dTilt.png',COUNT),'Resolution',300)
+end
+
+
+figure()
+hold on
+for COUNT = 1:length(normalizedTilt)
+    for season = 1:length(seasons)
+        scatter(X,normalizedTilt{COUNT}{season},color(COUNT),'filled')
+%         plot(X,normalizedTilt{COUNT}{season})
+    end
 %     nameit = sprintf('Tilt on Transceiver: %d',COUNT);
 %     title(nameit)
-%     ylabel('Normalized Det. Efficiency')
-%     xlabel('Instrument Tilt')
+    ylabel('Normalized Det. Efficiency')
+    xlabel('Instrument Tilt')
 %    lgd =  legend('Winter','Spring','Summer','Fall','Mariners Fall',loc='upper left')
-% %    lgd.Location = 'northwest';
-% %     exportgraphics(gcf,sprintf('Transceiver%dTilt.png',COUNT),'Resolution',300)
-% end
-% 
-% 
-% figure()
-% hold on
-% for COUNT = 1:length(normalizedTilt)
-%     for season = 1:length(seasons)
-%         scatter(X,normalizedTilt{COUNT}{season},color(COUNT),'filled')
-% %         plot(X,normalizedTilt{COUNT}{season})
-%     end
-% %     nameit = sprintf('Tilt on Transceiver: %d',COUNT);
-% %     title(nameit)
-%     ylabel('Normalized Det. Efficiency')
-%     xlabel('Instrument Tilt')
-% %    lgd =  legend('Winter','Spring','Summer','Fall','Mariners Fall',loc='upper left')
-% %    lgd.Location = 'northwest';
-% %     exportgraphics(gcf,sprintf('Transceiver%dTilt.png',COUNT),'Resolution',300)
-%     title('12 gauge shotgun blast of tilt vs efficiency')
-% end
+%    lgd.Location = 'northwest';
+%     exportgraphics(gcf,sprintf('Transceiver%dTilt.png',COUNT),'Resolution',300)
+    title('12 gauge shotgun blast of tilt vs efficiency')
+end
 
 %%
 % Wind direction instead of wind speed, here
@@ -211,15 +211,15 @@ x = 1:length(averageWindSpeedAnnual(1,:))
 seasonName = [{'Winter','Spring','Summer','Fall','Mariner''s Fall','Fall'}]
 color = ['r','r','g','g','k','k','b','b','m','m'];
 
-% figure()
-% hold on
-% for COUNT = 1:height(averageWindSpeedAnnual)
-%     plot(x,averageWindSpeedAnnual(COUNT,:),color(COUNT))
-% %     errorbar(x,averageWindSpeedAnnual(COUNT,:),errorWindAnnual(COUNT,:),color(COUNT),"LineStyle","none")
-% end
-% xlabel('Wind Magnitude')
-% ylabel('Det Efficiency')
-% ylim([0 6])
+figure()
+hold on
+for COUNT = 1:height(averageWindSpeedAnnual)
+    plot(x,averageWindSpeedAnnual(COUNT,:),color(COUNT))
+%     errorbar(x,averageWindSpeedAnnual(COUNT,:),errorWindAnnual(COUNT,:),color(COUNT),"LineStyle","none")
+end
+xlabel('Wind Magnitude')
+ylabel('Det Efficiency')
+ylim([0 6])
 % 
 % 
 % 
@@ -352,6 +352,45 @@ title('','Transmission Success')
 %pairings. 
 
 figure()
+nexttile()
+hold on
+for COUNT = 1:length(noiseCompareAnnual)
+    plot(x,noiseCompareAnnual{COUNT},color(COUNT),'LineWidth',2)
+end
+xlabel('Wind Magnitude (m/s)')
+ylabel('Ambient Sound (69 kHz, mV)')
+xlim([0 12])
+x1 = xline(3.60,'--','LineWidth',1.5)
+x2 = xline(5.65,'--','LineWidth',1.5)
+x3 = xline(10.8,'--','LineWidth',1.5)
+title('High Wind Magnitudes Enabling Acoustic Detections','Ambient Sound')
 
+for COUNT = 1:2:length(noiseCompareAnnual)-1
+    figure()
+    hold on
+    yyaxis left
+    plot(x,noiseCompareAnnual{COUNT},color(COUNT),'LineStyle','-','LineWidth',2);
+    plot(x,noiseCompareAnnual{COUNT+1},color(COUNT+1),'LineStyle','-','LineWidth',2);
+    ylim([400 800])
+    yyaxis right
+    plot(x,normalizedWSpeedAnnual(COUNT,:),color(COUNT),'LineStyle','--','LineWidth',2)
+    plot(x,normalizedWSpeedAnnual(COUNT+1,:),color(COUNT+1),'LineStyle','--','LineWidth',2)
+    ylim([0 1])
+end
 
+%Tiling transceiver pairings
 
+figure()
+tiledlayout(4,4,'TileSpacing','Compact','Padding','Compact')
+hold on
+for COUNT = 1:2:length(noiseCompareAnnual)-1
+    nexttile
+    yyaxis left
+    plot(x,noiseCompareAnnual{COUNT},color(COUNT),'LineStyle','-','LineWidth',2);
+    plot(x,noiseCompareAnnual{COUNT+1},color(COUNT+1),'LineStyle','-','LineWidth',2);
+    ylim([400 800])
+    yyaxis right
+    plot(x,normalizedWSpeedAnnual(COUNT,:),color(COUNT),'LineStyle','--','LineWidth',2)
+    plot(x,normalizedWSpeedAnnual(COUNT+1,:),color(COUNT+1),'LineStyle','--','LineWidth',2)
+    ylim([0 1])
+end
