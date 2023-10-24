41c6f9a890fc7d22a0a44b9c09d728b2448937fe
Frank McQuarrie <frankmac@uga.edu>
2023-10-24 18:24:53 -0400

    Update stats2020.m

--- stats2020.m
+++ stats2020.m
@@ -2,10 +2,17 @@
 
 binnedAVG
 
-
-%oneMatrix = [fullData{1,1}; fullData{1,2};fullData{1,3}; fullData{1,4};fullData{1,5}; fullData{1,6};fullData{1,7}; fullData{1,10}];
-oneMatrix =  [fullData{1,1}; fullData{1,2};fullData{1,3}; fullData{1,4};fullData{1,5}; fullData{1,6};fullData{1,7};fullData{1,8};fullData{1,9}; fullData{1,10}];
-
 cd 'C:\Users\fmm17241\OneDrive - University of Georgia\statisticalAnalysis'
 
-writetimetable(oneMatrix,'oneMatrix.csv')
+%Working on matrices
+oneMatrix =  [fullData{1,1}; fullData{1,2};fullData{1,3}; fullData{1,4};fullData{1,5}; fullData{1,6};fullData{1,7};fullData{1,8};fullData{1,9}; fullData{1,10}];
+
+for K = 1:length(fullData)
+    writetimetable(fullData{1,K},sprintf('matrix%d.csv',K))
+end
+
+
+
+
+
+% writetimetable(oneMatrix,'oneMatrix.csv')
