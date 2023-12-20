6192de1a3219054d9cb0a4bc8d40c1d68ba1c79e
Frank McQuarrie <frankmac@uga.edu>
2023-12-20 15:35:23 -0500

    Update frankFFT.m

--- frankFFT.m
+++ frankFFT.m
@@ -20,7 +20,9 @@ xlabel("t (milliseconds)")
 ylabel("X(t)")
 
 Y = fft(data)
-
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
 
 
 f = Fs/L*(0:(L/2));
@@ -52,68 +54,126 @@ title("Single-Sided Amplitude Spectrum of S(t)")
 xlabel("f (Hz)")
 ylabel("|P1(f)|")
 
-%%
+ %Franks Hanning Window Implementation
+
+Fs = 1000;            % Sampling frequency                    
+T = 1/Fs;             % Sampling period       
+L = 1500;             % Length of signal
+t = (0:L-1)*T;        % Time vector
+f = Fs/L*(0:(L/2));
+
+S = 0.8 + 0.7*sin(2*pi*50*t) + sin(2*pi*120*t);
+
+data = S + 2*randn(size(t));
+hannWind = hanning(100);
 
 
-Fs = 1000;                    % Sampling frequency
-T = 1/Fs;                     % Sampling period
-L = 1000;                     % Length of signal
-t = (0:L-1)*T;                % Time vector
-
-x1 = cos(2*pi*50*t);          % First row wave
-x2 = cos(2*pi*150*t);         % Second row wave
-x3 = cos(2*pi*300*t);         % Third row wave
-
-data = [x1; x2; x3];
+dataTest = data.*hannWind;
 
 figure()
-for i = 1:3
-    subplot(3,1,i)
-    plot(t(1:100),data(i,1:100))
-    title("Row " + num2str(i) + " in the Time Domain")
-end
+wvtool(hannWind)
 
 
-dim = 2;
-Y = fft(data,L,dim);
+Ywindow = fft(dataTest);
+P2window = abs(Ywindow/L);
+P1window = P2window(1:L/2+1);
+P1window(2:end-1) = 2*P1window(2:end-1);
+figure()
+plot(f,P1window,"LineWidth",3) 
+title("Single-Sided Amplitude Spectrum of S(t)")
+xlabel("f (Hz)")
+ylabel("|P1(f)|")
+
+
+
+
+Y = fft(data);
 P2 = abs(Y/L);
-P1 = P2(:,1:L/2+1);
-P1(:,2:end-1) = 2*P1(:,2:end-1);
-
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
 figure()
-for i=1:3
-    subplot(3,1,i)
-    plot(0:(Fs/L):(Fs/2-Fs/L),P1(i,1:L/2))
-    title("Row " + num2str(i) + " in the Frequency Domain")
-end
+plot(f,P1,"LineWidth",3) 
+title("Single-Sided Amplitude Spectrum of S(t)")
+xlabel("f (Hz)")
+ylabel("|P1(f)|")
+
+
+
 
 %%
 % Okay, gotta experiment with my data
 % 1hz = 1 per second
 % 1/3600 Hz = 1 per hour? I think
 % Frank ran "deepCutz"
+deepCutz
+clearvars -except receiverData oneDrive githubToolbox
+close all
 
-Fs = 1/3600;        % Sampling Frequency
+%
+Fs = 1/3600;        % Sampling Frequency, hourly
 T = 1/Fs            % Sample Period
-L = length(receiverData{5}.HourlyDets)          % Length of Signal
+L = length(receiverData{4}.HourlyDets)          % Length of Signal
 % L = length(receiverData{5}.HourlyDets)
 t = (0:L-1)*T;
 
 %Testing my data
-T = receiverData{5}.DT
+T = receiverData{4}.DT
 % T2 = receiverData{5}.DT
-data = receiverData{5}.HourlyDets;
-% X2 = receiverData{5}.HourlyDets;
-
-
+data = receiverData{4}.HourlyDets';
+window = hanning(100);
+f = Fs/L*(0:(L/2));
 
 figure()
 plot(T,data)
 title("Row in the Time Domain")
 
 %
+
+dataTest = data.*window;
+
+
+Ywindow = fft(dataTest);
+P2window = abs(Ywindow/L);
+P1window = P2window(1:L/2+1);
+P1window(2:end-1) = 2*P1window(2:end-1);
+figure()
+plot(f,P1window,"LineWidth",3) 
+title("Single-Sided Amplitude Spectrum, Data: 40-Hour  Hanning Window")
+xlabel("f (Hz)")
+ylabel("|P1(f)|")
+
+
+
+
+Y = fft(data);
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+figure()
+plot(f,P1,"LineWidth",3) 
+title("Single-Sided Amplitude Spectrum")
+xlabel("f (Hz)")
+ylabel("|P1(f)|")
+
+
+
+
+
+
+
+
+
+
+
+
+
+%%
+
+
+
+
 Y = fft(data,L);
-% Y2 = fft(X2,L2);
+windowY = fft(window.*data,:)
 
 figure()
 plot(Fs/L*(0:L-1),abs(Y),"LineWidth",3)
@@ -142,6 +202,11 @@ xlabel("f (Hz)")
 ylabel("|P1(f)|")
 
 
+
+
+
+
+
 %%
 %Off reef is super obvious: tides and daily.
 %On reef is not. Need to look at lowpass/highpass filters, or windowing.
@@ -152,7 +217,6 @@ L = length(receiverData{5}.HourlyDets)          % Length of Signal
 t = (0:L-1)*T;
 
 
-clearvars
 %Testing my data
 T = receiverData{4}.DT
 % T2 = receiverData{5}.DT
@@ -231,6 +295,43 @@ ylabel("|P1(f)|")
 
 
 
+
+
+
+%%
+
+
+Fs = 1000;                    % Sampling frequency
+T = 1/Fs;                     % Sampling period
+L = 1000;                     % Length of signal
+t = (0:L-1)*T;                % Time vector
+
+x1 = cos(2*pi*50*t);          % First row wave
+x2 = cos(2*pi*150*t);         % Second row wave
+x3 = cos(2*pi*300*t);         % Third row wave
+
+data = [x1; x2; x3];
+
+figure()
+for i = 1:3
+    subplot(3,1,i)
+    plot(t(1:100),data(i,1:100))
+    title("Row " + num2str(i) + " in the Time Domain")
+end
+
+
+dim = 2;
+Y = fft(data,L,dim);
+P2 = abs(Y/L);
+P1 = P2(:,1:L/2+1);
+P1(:,2:end-1) = 2*P1(:,2:end-1);
+
+figure()
+for i=1:3
+    subplot(3,1,i)
+    plot(0:(Fs/L):(Fs/2-Fs/L),P1(i,1:L/2))
+    title("Row " + num2str(i) + " in the Frequency Domain")
+end
 
 
 
