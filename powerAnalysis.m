e6ba14ccf9574c9c302d01d604692155576b30cd
Frank McQuarrie <frankmac@uga.edu>
2024-01-15 14:46:41 -0500

    Update powerAnalysis.m

--- powerAnalysis.m
+++ powerAnalysis.m
@@ -1,18 +1,67 @@
 buildReceiverData
-
+clearvars -except receiverData oneDrive githubToolbox
 close all
 
 %
 %Brock's Code: data, # of bins, Detrend?, Window?, Interval (secs), cutoff
 % dataout=Power_spectra(datainA,bins,DT,windoww,samplinginterval,cutoff)
-signalNoise = receiverData{4}.Noise;
-signalDets  =   receiverData{4}.HourlyDets;
-signalPings  =   receiverData{4}.Pings;
-signalWinds  =   receiverData{4}.windSpd;
+for COUNT = 1:length(receiverData)
+    signalNoise{COUNT}  =   receiverData{COUNT}.Noise;
+    signalDets{COUNT}   =   receiverData{COUNT}.HourlyDets;
+    signalPings{COUNT}  =   receiverData{COUNT}.Pings;
+    signalWinds{COUNT}  =   receiverData{COUNT}.windSpd;
+end
 
 Fs = (2*pi)/(60*60);            % Sampling frequency, 1 sample every 60 minutes. Added 2pi.
 FsPerDay = Fs*86400;
 
+for COUNT =  1:length(receiverData)
+    Y{COUNT} = fft(signalNoise{COUNT})              % FFT of the processed signals 
+    L{COUNT} = height(signalNoise{COUNT})        % Length of signal
+    magnitude{COUNT} = abs(Y{COUNT});
+    averageWindowOutput{COUNT}(:,1) = mean(Y{COUNT},2); %Averaging all my windows
+end
+
+
+for COUNT = 1:length(receiverData)
+    figure(COUNT)
+    % plot(FsPerDay/L{1}*(0:L{1}-1),abs(rawSignalProcess.*conj(rawSignalProcess)),'r',"LineWidth",3)
+    plot(FsPerDay/L{COUNT}*(0:L{COUNT}-1),abs(averageWindowOutput{COUNT}),'r',"LineWidth",3)    
+    title("", "FFT Output, Log")
+    xlabel("f (Hz)")
+    ylabel("|fft(X)|")
+    set(gca,'XScale','log')
+    set(gca,'YScale','log')
+end
+
+
+%%
+%Processing
+
+for COUNT =  1:length(receiverData)
+    signalNoiseProcessed{COUNT} = buffer(signalNoise{COUNT},40,20);  
+    signalNoiseProcessed{COUNT} = padarray(signalNoiseProcessed{COUNT},height(signalNoiseProcessed{COUNT})*2,'post');
+end
+
+
+for COUNT =  1:length(receiverData)
+    Y{COUNT} = fft(signalNoiseProcessed{COUNT})              % FFT of the processed signals 
+    L{COUNT} = height(signalNoiseProcessed{COUNT})        % Length of signal
+    magnitude{COUNT} = abs(Y{COUNT});
+    averageWindowOutput{COUNT}(:,1) = mean(Y{COUNT},2); %Averaging all my windows
+end
+
+
+for COUNT = 1:length(receiverData)
+    figure(COUNT)
+    % plot(FsPerDay/L{1}*(0:L{1}-1),abs(rawSignalProcess.*conj(rawSignalProcess)),'r',"LineWidth",3)
+    plot(FsPerDay/L{COUNT}*(0:L{COUNT}-1),abs(averageWindowOutput{COUNT}),'r',"LineWidth",3)    
+    title("", "FFT Output, Log")
+    xlabel("f (Hz)")
+    ylabel("|fft(X)|")
+    set(gca,'XScale','log')
+    set(gca,'YScale','log')
+end
 
 
 
