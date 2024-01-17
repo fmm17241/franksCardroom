1903cd35c2c3452636fe8666b2a968c8cfbd4a1b
Frank McQuarrie <frankmac@uga.edu>
2024-01-17 11:46:58 -0500

    Update SpringNeapSpectral.m

--- SpringNeapSpectral.m
+++ SpringNeapSpectral.m
@@ -1,3 +1,19 @@
 %Frank tried FFTs in powerAnalysis, now I want to compare
 % at different tides. We'll see, IDK anymore.
 
+% Load in data to plot
+buildReceiverData
+
+%%
+% Find Spring/Neap cycle. I want to find chunks of time where the tides
+% were strong and weak.
+figure()
+plot(receiverData{1}.DT,receiverData{1}.crossShore)
+ylabel('X-Shore (m/s)')
+title('Finding Strong and Weak Tides')
+
+
+%Winter weak: weak weak tides Feb 29 - Mar 05
+%Winter Strong: Mar 8 - Mar 14
+%Summer Weak: weak weak tides Aug 8 - Aug 15
+%Summer strong: Jun 03 - Jun 09
