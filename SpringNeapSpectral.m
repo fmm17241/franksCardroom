7791d3f7596857b4b56a1f06665b3dddde6bc9bc
Frank McQuarrie <frankmac@uga.edu>
2024-01-18 11:42:07 -0500

    Update SpringNeapSpectral.m

--- SpringNeapSpectral.m
+++ SpringNeapSpectral.m
@@ -14,8 +14,10 @@ title('Finding Strong and Weak Tides')
 
 
 %Winter weak: weak weak tides Feb 29 - Mar 05
+receiverData{1}.DT(728-871);
 %Winter Strong: Mar 8 - Mar 14
 %Summer Weak: weak weak tides Aug 8 - Aug 15
 %Summer strong: Jun 03 - Jun 09
+%Summer weak: Jul 13 - Jul 17
 %Summer middle: Jul 20 - Jul 25
 %Weak: Sep 8 - Sep 13
