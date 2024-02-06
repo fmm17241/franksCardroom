29c242bc1650cc4e8cbb0ed9c80df76d5ffca348
Frank McQuarrie <frankmac@uga.edu>
2024-02-06 11:21:01 -0500

    Create rowellSounds.m

---
+++ rowellSounds.m
@@ -0,0 +1,40 @@
+cd 'C:\Users\fmac4\OneDrive - University of Georgia\data\passiveSounds'
+
+%Lists the files in the directory chosen above.
+broadBandFiles = dir('*BB*');
+octaveFiles = dir('*_ol*');
+oneThirdOctaveFiles = dir('*tol*');
+psdFiles = dir('*psd*');
+
+
+%
+%Open the files and creates the data
+%Broadband
+fid = fopen(broadBandFiles.name);
+indata = textscan(fid, '%s%s', 'HeaderLines',1);
+fclose(fid);
+BBdata = [indata{1}, indata{2}];
+clear indata
+
+% One Octave
+fid = fopen(octaveFiles.name);
+indata = textscan(fid, '%s%s%s%s%s%s%s%s%s%s%s', 'HeaderLines',1);
+fclose(fid);
+OLdata = [indata{1}, indata{2}, indata{3}, indata{4}, indata{5}, indata{6}, indata{7}, indata{8}, indata{9}, indata{10}, indata{11}];
+clear indata
+
+% 1/3rd Octave
+fid = fopen(oneThirdOctaveFiles.name);
+indata = textscan(fid, '%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s', 'HeaderLines',1);
+fclose(fid);
+TOLdata = [indata{1}, indata{2}];
+clear indata
+
+% PSD
+fid = fopen(psdFiles.name);
+indata = textscan(fid, '%s%s', 'HeaderLines',1);
+fclose(fid);
+PSDdata = [indata{1}, indata{2}];
+clear indata
+
+
