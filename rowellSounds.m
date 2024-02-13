4ab654e84ba8266fbbca5cab85da1791b43611ba
Frank McQuarrie <frankmac@uga.edu>
2024-02-12 21:51:05 -0500

    add
    add

--- rowellSounds.m
+++ rowellSounds.m
@@ -123,38 +123,40 @@ title('Windspeed vs High (blue) and Low (red) Frequencies','Detrended Noise and 
 figure()
 tiledlayout(4,2,'TileSpacing','Compact','TileIndexing', 'columnmajor')
 ax1 = nexttile([2 1])
-plot(receiverData{4}.DT,receiverData{4}.windSpd)
+plot(receiverData{4}.DT,receiverData{4}.windSpd,'k')
 hold on
 ylabel('Windspeed (m/s)')
-y1 = yline(6,'--','Moderate Breeze')
-yl.LabelHorizontalAlignment = 'center';
+y3 = yline(6,'--','Moderate Breeze')
+y3.LabelHorizontalAlignment = 'left';
 y2 = yline(10,'--','Strong Breeze')
 y2.LabelHorizontalAlignment = 'center';
 title('Wind''s Effect on a Shallow Coastal Reef''s Soundscape','Wind Magnitude')
 
 ax2 = nexttile([1 1])
-plot(octaveLevel{4}.Time,octaveLevel{4}.Var1(:,4),'r')
+plot(octaveLevel{4}.Time,octaveLevel{4}.Var1(:,4),'b','LineWidth',2)
 ylabel({'Sound Pressure Level'; 'Hourly Median (dB re 1 µPa)'})
 title('','Low-Frequency Noise (0.17-0.36 kHz)')
 
 ax3 = nexttile([1 1])
-plot(octaveLevel{4}.Time,octaveLevel{4}.Var1(:,10),'b')
+plot(octaveLevel{4}.Time,octaveLevel{4}.Var1(:,10),'r','LineWidth',2)
 ylabel({'Sound Pressure Level'; 'Hourly Median (dB re 1 µPa)'})
 title('','High-Frequency Noise (11-22 kHz)')
 
 linkaxes([ax1 ax2 ax3],'X')
 
 ax4 = nexttile([2 1])
-scatter(receiverData{4}.windSpd(1678:4371,:),octaveLevel{4}.Var1(:,4))
+scatter(receiverData{4}.windSpd(1678:4371,:),octaveLevel{4}.Var1(:,4),'b')
 ylabel('Sound Pressure Level')
 xlabel('Windspeed (m/s')
-title('Sound Pressures vs Increasing Windspeed','Low-Frequency')
+title('Low-Frequency Noise vs Increasing Windspeed')
+% title('Sound Pressures vs Increasing Windspeed','Low-Frequency')
 
 ax5 = nexttile([2 1])
-scatter(receiverData{4}.windSpd(1678:4371,:),octaveLevel{4}.Var1(:,10))
+scatter(receiverData{4}.windSpd(1678:4371,:),octaveLevel{4}.Var1(:,10),'r')
 ylabel('Sound Pressure Level')
 xlabel('Windspeed (m/s')
-title('','High-Frequency')
+title('High-Frequency Noise vs Increasing Windspeed')
+% title('','High-Frequency')
 
 
 %%
