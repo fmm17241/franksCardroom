3d86f50c20725350b6dce00b3cca6d368a0ff1e0
Frank McQuarrie <frankmac@uga.edu>
2024-02-28 21:09:51 -0500

    Update newFrank.m

--- newFrank.m
+++ newFrank.m
@@ -10,20 +10,37 @@ clearvars -except receiverData hourlyDetections mooredReceivers oneDrive githubT
 close all
 
 sunkenReef = receiverData{5};
-flatReef   = receiverData{4};
+flatReef{1}   = receiverData{4};
+flatReef{2}   = receiverData{1};
+flatReef{3}   = receiverData{2};
+flatReef{4}   = receiverData{13};
+
 
 hillToSunken = hourlyDetections{11};
 hillToFlat   = hourlyDetections{14};
 
-clearvars -except sunkenReef flatReef hillTo* oneDrive githubToolbox
+clearvars -except receiverData sunkenReef flatReef hillTo* oneDrive githubToolbox
 close all
 
 %Okay. Two scenarios to compare, quantitatively
-sunkenLM = fitlm(sunkenReef.windSpd,sunkenReef.Noise)
-flatLM   = fitlm(flatReef.windSpd,flatReef.Noise)
 
-sunkenLM = fitlm(sunkenReef.Temp,sunkenReef.Noise)
-flatLM   = fitlm(flatReef.Temp,flatReef.Noise)
+sunkenLMWindNoise = fitlm(sunkenReef.windSpd,sunkenReef.Noise)
+sunkenLMWindDets = fitlm(sunkenReef.windSpd,sunkenReef.HourlyDets)
+sunkenLMTempNoise = fitlm(sunkenReef.Temp,sunkenReef.Noise)
+
+for COUNT = 1:4
+    flatLMWindNoise{COUNT}   = fitlm(flatReef{COUNT}.windSpd,flatReef{COUNT}.Noise)
+    flatLMTempNoise{COUNT}   = fitlm(flatReef{COUNT}.Temp,flatReef{COUNT}.Noise)
+    flatLMWindDets{COUNT}   = fitlm(flatReef{COUNT}.windSpd,flatReef{COUNT}.HourlyDets)
+end
+
+
+
+
+
+
+
+
 
 
 
