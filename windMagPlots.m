6845a253a8aad966c2de66ee54d60f74b3087001
Frank McQuarrie <frankmac@uga.edu>
2024-03-19 16:10:41 -0400

    Update windMagPlots.m

--- windMagPlots.m
+++ windMagPlots.m
@@ -104,48 +104,6 @@ end
 % end
 
 
-figure()
-hold on
-for COUNT = 1:length(normalizedTilt)
-    for season = 1:length(seasons)
-        scatter(X,normalizedTilt{COUNT}{season},color(COUNT),'filled')
-%         plot(X,normalizedTilt{COUNT}{season})
-    end
-%     nameit = sprintf('Tilt on Transceiver: %d',COUNT);
-%     title(nameit)
-    ylabel('Normalized Det. Efficiency')
-    xlabel('Instrument Tilt')
-%    lgd =  legend('Winter','Spring','Summer','Fall','Mariners Fall',loc='upper left')
-%    lgd.Location = 'northwest';
-%     exportgraphics(gcf,sprintf('Transceiver%dTilt.png',COUNT),'Resolution',300)
-    title('12 gauge shotgun blast of tilt vs efficiency')
-end
-
-%
-% Wind direction instead of wind speed, here
-cd (localPlots)
-
-seasonName = [{'Winter','Spring','Summer','Fall','Mariner''s Fall','Fall'}]
-color = ['r','r','g','g','k','k','b','b','m','m'];
-
-
-X = 20:20:360;
-
-figure()
-scatter(X,yearlywindDir,'filled')
-ylabel('Normalized Det. Efficiency')
-xlabel('Wind Direction')
-title('Wind Direction''s Enabling of Detection Success')
-
-
-figure()
-scatter(X,yearlyTiltVswindDir,'filled')
-xlabel('Wind Direction')
-ylabel('Instrument Tilt')
-title('Phys Processes Tilting Instruments')
-
-
-
 
 
 for COUNT = 1:length(normalizedwindDir)
@@ -165,45 +123,6 @@ for COUNT = 1:length(normalizedwindDir)
     title('WindDir vs efficiency')
 end
 
-% 
-pairingNumb = [1;1;2;2;3;3;4;4;5;5]
-for COUNT = 1:2:length(normalizedwindDir)
-    f = figure;
-    f.Position = [100 100 450 800];
-    tiledlayout(5,1,'TileSpacing','Compact','Padding','Compact')
-    for season = 1:length(seasons)
-        nexttile()
-        scatter(X,normalizedwindDir{COUNT}{season},'r','filled')
-        hold on
-        scatter(X,normalizedwindDir{COUNT+1}{season},'k','filled')
-        nameit = sprintf('Wind, Transceivers %d, %s',pairingNumb(COUNT),seasonName{season});
-        title(nameit)
-        hold off
-    end
-    ylabel('Normalized Det. Efficiency')
-    xlabel('Wind Direction')
-%     exportgraphics(gcf,sprintf('WindTrans%dBOTHdirections.png',pairingNumb(COUNT)),'Resolution',300)
-end
-% 
-
-for COUNT = 1:length(normalizedwindDir)
-    figure()
-    hold on
-    for season = 1:length(seasons)
-        scatter(X,normalizedwindDir{COUNT}{season},color(COUNT),'filled')
-%         plot(X,normalizedTilt{COUNT}{season})
-    end
-%     nameit = sprintf('Tilt on Transceiver: %d',COUNT);
-%     title(nameit)
-    ylabel('Normalized Det. Efficiency')
-    xlabel('Wind Dir.')
-%    lgd =  legend('Winter','Spring','Summer','Fall','Mariners Fall',loc='upper left')
-%    lgd.Location = 'northwest';
-%     exportgraphics(gcf,sprintf('Transceiver%dTilt.png',COUNT),'Resolution',300)
-    title('WindDir vs efficiency')
-end
-
-%
 
 % Annuals
 
@@ -281,43 +200,36 @@ ylabel('WaveHeight')
 title('WaveHeight','Increasing Wind Magnitude')
 
 %
-x = 0:12;
 
-figure()
-scatter(x,yearlyWindSpeed,200,yearlyStratVsWindSpeed,'filled')
-xlim([-0.5 12.3])
-h = colorbar
-colormap jet
-ylabel(h,'Bulk Thermal Strat (°C)')
-xlabel('Wind Magnitude (m/s)')
-ylabel('Normalized Detection Efficiency')
-title('Increasing Wind Magnitude','Change in Bulk Thermal Stratification')
-
-
-%FM 7/19 tiled layout for figure
 
 x = 1:length(averageWindSpeedAnnual(1,:))
 seasonName = [{'Winter','Spring','Summer','Fall','Mariner''s Fall','Fall'}]
 color = ['r','r','g','g','k','k','b','b','m','m'];
 
+% Frank adding colors so I can track who is who
+myColors = ['b','r','y','k','m','g','b','r','y','k','m','g','b']
+myLineStyles = ["-";"--";"-o"]
+
 
 f = figure;
 f.Position = [100 -50 500 500];
 % tiledlayout(1,3,'TileSpacing','Compact','Padding','Compact')
-tiledlayout(1,3,'TileSpacing','Compact','Padding','Compact')
+tiledlayout(1,4,'TileSpacing','Compact','Padding','Compact')
 % 
-% nexttile()
-% hold on
-% for COUNT = 1:height(averageWindSpeedAnnual)
-%     plot(x,wavesCompareAnnual{COUNT},'k','LineWidth',2)
-% end
-% xline(3.60,'--','LineWidth',1.5,'label','Light Breeze')
-% xline(5.65,'--','LineWidth',1.5,'label','Moderate Breeze')
-% x1 = xline(10.8,'--','LineWidth',1.5,'label','Strong Breeze')
-% x1.LabelVerticalAlignment = 'bottom'; x1.LabelHorizontalAlignment = 'left';
-% xlim([0 12])
-% ylabel('WaveHeight')
-% title('','WaveHeight')
+nexttile()
+hold on
+for COUNT = 1:height(averageWindSpeedAnnual)
+    plot(x,wavesCompareAnnual{COUNT},'k','LineWidth',2)
+end
+xline(3.60,'--','LineWidth',1.5,'label','Light Breeze')
+xline(5.65,'--','LineWidth',1.5,'label','Moderate Breeze')
+x1 = xline(10.8,'--','LineWidth',1.5,'label','Strong Breeze')
+x1.LabelVerticalAlignment = 'bottom'; x1.LabelHorizontalAlignment = 'left';
+xlim([0 12])
+ylabel('WaveHeight')
+title('','WaveHeight')
+
+
 
 nexttile()
 hold on
@@ -332,7 +244,8 @@ x2.LabelVerticalAlignment = 'bottom'; x1.LabelHorizontalAlignment = 'left';
 xlim([0 12])
 ylabel('Stratification (Δ°C)')
 title('','Bulk Thermal Strat.')
-
+ax = gca;
+ax.LineStyleOrder = myLineStyles
 
 nexttile()
 hold on
@@ -364,6 +277,70 @@ xline(5.65,'--','LineWidth',1.5)
 x1 = xline(10.8,'--','LineWidth',1.5)
 title('','Transmission Success')
 
+
+%% Okay Frank, make separate figs. Ugh.
+for COUNT = 1:height(averageWindSpeedAnnual)
+f = figure;
+f.Position = [100 -50 500 500];
+% tiledlayout(1,3,'TileSpacing','Compact','Padding','Compact')
+tiledlayout(1,4,'TileSpacing','Compact','Padding','Compact')
+% 
+nexttile()
+hold on
+    plot(x,wavesCompareAnnual{COUNT},'k','LineWidth',2)
+xline(3.60,'--','LineWidth',1.5,'label','Light Breeze')
+xline(5.65,'--','LineWidth',1.5,'label','Moderate Breeze')
+x1 = xline(10.8,'--','LineWidth',1.5,'label','Strong Breeze')
+x1.LabelVerticalAlignment = 'bottom'; x1.LabelHorizontalAlignment = 'left';
+xlim([0 12])
+ylabel('WaveHeight')
+title('','WaveHeight')
+
+
+
+nexttile()
+hold on
+    plot(x,stratCompareWindAnnual{COUNT},'LineWidth',2)
+x1 = xline(3.60,'--','LineWidth',1.5,'label','Light Breeze')
+x2 = xline(5.65,'--','LineWidth',1.5,'label','Moderate')
+x3 = xline(10.8,'--','LineWidth',1.5,'label','Strong Breeze')
+x1.LabelVerticalAlignment = 'bottom'; x1.LabelHorizontalAlignment = 'left';
+x2.LabelVerticalAlignment = 'bottom'; x1.LabelHorizontalAlignment = 'left';
+xlim([0 12])
+ylabel('Stratification (Δ°C)')
+title('','Bulk Thermal Strat.')
+ax = gca;
+ax.LineStyleOrder = myLineStyles
+
+nexttile()
+hold on
+    plot(x,noiseCompareAnnual{COUNT},'LineWidth',2)
+xlabel('Wind Magnitude (m/s)')
+ylabel('High-Freq. Sound (mV)')
+xlim([0 12])
+x1 = xline(3.60,'--','LineWidth',1.5)
+x2 = xline(5.65,'--','LineWidth',1.5)
+x3 = xline(10.8,'--','LineWidth',1.5)
+title('Coastal Wind''s Effect on Shallow Reef Telemetry','50-100 kHz Noise')
+
+
+
+nexttile()
+hold on
+    plot(x,normalizedWSpeedAnnual(COUNT,:),'LineWidth',2)
+xlim([0 12])
+
+
+ylabel('Normalized Det Efficiency')
+xline(3.60,'--','LineWidth',1.5)
+xline(5.65,'--','LineWidth',1.5)
+x1 = xline(10.8,'--','LineWidth',1.5)
+title('','Transmission Success')
+
+end
+
+
+%%
 
 %
 % FM 7/23/23 - have to look at single plots of noise vs detection efficiency
