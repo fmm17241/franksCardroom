c51a29f528a26b15bd318dcb3e5f9ec77fe47b58
Frank McQuarrie <frankmac@uga.edu>
2024-04-05 15:53:59 -0400

    Update surfaceLossFM.m

--- surfaceLossFM.m
+++ surfaceLossFM.m
@@ -1,9 +1,13 @@
 %FM 4/2024
 %Studying the surface loss code from the acoustics toolbox
-x = 1: 5;
+x = 1: 13;
 theta = 90;
 freq = 69;
-U = [0 2 5 8 12]
+U = [0 1 2 3 4 5 6 7 8 9 10 11 ...
+    12 ]
 
 
 calculatedLoss = SurfLoss(theta, U, freq)
+
+figure()
+plot(x,calculatedLoss)
