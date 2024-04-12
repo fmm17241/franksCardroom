1e779a60d85df289ccea46526207c629e0e4e683
Frank McQuarrie <frankmac@uga.edu>
2024-04-12 16:48:14 -0400

    Update surfaceLossFM.m

--- surfaceLossFM.m
+++ surfaceLossFM.m
@@ -1,15 +1,16 @@
 %FM 4/2024
 %Studying the surface loss code from the acoustics toolbox
-x = 1: 6;
+U = 2:0.1:12;
 AcuteTheta = 45;
 ObtuseTheta = 110;
+BasicAngle = 90;
 LowFreq = 0.6
 MidFreq = 2
 HighFreq = 69;
 
 
-U = [4 10]
-
+calculatedHighFreq      = SurfLoss(BasicAngle, U, HighFreq)
+calculatedLowFreq      = SurfLoss(BasicAngle, U, LowFreq)
 
 calculatedHighFreqAcute = SurfLoss(AcuteTheta, U, HighFreq)
 calculatedHighFreqObtuse = SurfLoss(ObtuseTheta, U, HighFreq)
@@ -19,9 +20,9 @@ calculatedLowFreqAcute = SurfLoss(AcuteTheta, U, LowFreq)
 calculatedLowFreqObtuse = SurfLoss(ObtuseTheta, U, LowFreq)
 
 figure()
-plot(x,calculatedHighFreqLoss,'r')
+plot(U,calculatedHighFreq,'r','LineWidth',2)
 hold on
-plot(x,calculatedLowFreqLoss,'b')
+plot(U,calculatedLowFreq,'--ob','LineWidth',2)
 legend('High Frequency','Low Frequency')
 
 
