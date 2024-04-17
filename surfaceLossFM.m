715f84e129725707be3e8fce7b8a8322a387d9fe
Frank McQuarrie <frankmac@uga.edu>
2024-04-17 09:13:41 -0400

    Update surfaceLossFM.m

--- surfaceLossFM.m
+++ surfaceLossFM.m
@@ -1,6 +1,6 @@
 %FM 4/2024
 %Studying the surface loss code from the acoustics toolbox
-x = 1: 6;
+x = 2:2:10;
 AcuteTheta = 45;
 ObtuseTheta = 110;
 LowFreq = 0.6
@@ -8,7 +8,7 @@ MidFreq = 2
 HighFreq = 69;
 
 
-U = [4 10]
+U = [2 4 6 8 10]
 
 
 calculatedHighFreqAcute = SurfLoss(AcuteTheta, U, HighFreq)
@@ -19,9 +19,12 @@ calculatedLowFreqAcute = SurfLoss(AcuteTheta, U, LowFreq)
 calculatedLowFreqObtuse = SurfLoss(ObtuseTheta, U, LowFreq)
 
 figure()
-plot(x,calculatedHighFreqLoss,'r')
+plot(x,calculatedHighFreqAcute,'r')
 hold on
-plot(x,calculatedLowFreqLoss,'b')
+plot(x,calculatedLowFreqAcute,'b')
 legend('High Frequency','Low Frequency')
+ylabel('Noise Loss (dBs)')
+xlabel('Windspeed')
+title('Loss of Noise at the Surface due to Bubbles')
 
 
