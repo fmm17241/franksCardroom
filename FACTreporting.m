695ce559036b929fb89d20d319f89dc25025cca7
Frank McQuarrie <frankmac@uga.edu>
2024-06-14 12:40:51 -0400

    updating toolbox
    sdf

--- FACTreporting.m
+++ FACTreporting.m
@@ -1,7 +1,7 @@
 %Frank figuring out how to efficiently report our detections
 
 % vemdir = 'C:\Users\fmm17241\OneDrive - University of Georgia\data\Glider\Data\Vemco'
-vemdir = 'C:\Users\fmm17241\OneDrive - University of Georgia\data\Glider\Data\Vemco\angus20190621'
+vemdir = 'C:\Users\fmm17241\OneDrive - University of Georgia\data\Glider\Data\Vemco\franklin20190621'
 
 %Starting line: feed it where the .vem files are.
 [vems] = vemProcess(vemdir)
@@ -9,11 +9,11 @@ vemdir = 'C:\Users\fmm17241\OneDrive - University of Georgia\data\Glider\Data\Ve
 
 
 %Now, have to contextualize them.
+load Deployment_May_2019_franklin_sbds.mat
+load Deployment_May_2019_franklin_tbds.mat
 
 
 
-
-
-[transmitters, correctedDN,correctedLat,correctedLon,correctedGPS,scidn,...
-    temperature,density,depth,pressure,salt,speed] = processDetections(fstruct,sstruct,vems)
+[detections, detectionReporting, detectionDN,detectionLat,detectionLon,detectionGPS,gliderDN,...
+    temperature,density,depth,pressure,salt,speed] = processDetections(fstruct,sstruct,vems);
 
--- processDetections.m
+++ processDetections.m
@@ -1,17 +1,20 @@
 
-% Must have already run vemprocess on the correct vemco directory.
+% Must have already run vemProcess on the correct vemco directory.
+%Then, load in correct glider data from that mission. This can be done in
+%real-time or afterwards.
+
 % fstruct = dbd or sbd, glider data. sstruct = ebd or tbd, glider data. Vems = vem structure
 
-function [transmitters, correctedDN,correctedLat,correctedLon,correctedGPS,scidn,...
+function [detections, detectionReporting, detectionDN,gliderLat,gliderLon,gliderGPS,gliderDN,...
     temperature,density,depth,pressure,salt,speed] = processDetections(fstruct,sstruct,vems)
 
-% dtdet = vems.dn;
-dtrec = vems.mooredDN;
+%Detection datenums from glider
+detectionTime = vems.dn;
 
-
-scidn = sstruct.dn;
-[~,ind2,~] = unique(scidn);
-scidn = scidn(ind2);
+%Glider data cleanup; finds unique time values without NaNs.
+gliderDN = sstruct.dn;
+[~,ind2,~] = unique(gliderDN);
+gliderDN = gliderDN(ind2);
 temperature = sstruct.sci_water_temp(ind2);
 cond = sstruct.sci_water_cond(ind2);
 cndr = ((cond)/(sw_c3515/10));
@@ -21,8 +24,7 @@ index = ~(isnan(temperature) | isnan(pressure)| isnan(cond));
 temperature = temperature(index);
 pressure = pressure(index);
 latmean = nanmean(fstruct.m_gps_lat);
-scidn = scidn(index);
-% scidn = sstruct.dn(index);
+gliderDN = gliderDN(index);
 cndr = cndr(index);
 salt = sw_salt(cndr,temperature,pressure);
 salt(imag(salt) ~= 0) = 0;
@@ -30,96 +32,63 @@ density = sw_dens(salt,temperature,pressure);
 depth = sw_dpth(pressure,latmean);
 speed = sndspd(salt,temperature,depth);
 
-% saltdet = interp1(scidn,salt,dtdet);
-% tempdet = interp1(scidn,temperature, dtdet);
-% pressdet = interp1(scidn,pressure,dtdet);
-% densdet = interp1(scidn,density,dtdet);
-% depthdet = interp1(scidn,depth,dtdet);
-transmittersalt = interp1(scidn,salt,dtrec);
-transmittertemp = interp1(scidn,temperature,dtrec);
-transmitterpress = interp1(scidn,pressure,dtrec);
-transmitterdensity = interp1(scidn,density,dtrec);
-transmitterdepth = interp1(scidn,depth,dtrec);
-
-dn = fstruct.dn;
-x = fstruct.m_gps_lat;
-y = fstruct.m_gps_lon;
-% Strip nans; lat and lon full of them. Around 1 coord set every 4 hours
-index = ~(isnan(x) | isnan(y) | isnan(dn));
-
-nanx = isnan(x);
-t = 1:numel(x);
-
-x(nanx) = interp1(t(~nanx),x(~nanx),t(nanx));
-y(nanx) = interp1(t(~nanx),y(~nanx),t(nanx));
+%Take the GPS points from the flight side of the glider.
+rawDN = fstruct.dn;
+rawLat = fstruct.m_gps_lat;
+rawLon = fstruct.m_gps_lon;
 
 
-[~, u] = unique(fstruct.dn);
-udn = fstruct.dn(u);
-% uvx = fstruct.m_final_water_vx(u);
-% uvy = fstruct.m_final_water_vy(u);
-% waterdepthtemp = fstruct.m_water_depth(u);
-xuniq = x(u);
-yuniq = y(u);
-index = ~(isnan(udn) | isnan(xuniq));
-dn = fstruct.dn(index);
-% waterdepth = waterdepthtemp(index);
-% dt = datetime(dn,'ConvertFrom','datenum');
-% vx = fstruct.m_final_water_vx(index);
-% vy = fstruct.m_final_water_vy(index);
-% vx = fillmissing(vx,'next');
-% vy = fillmissing(vy,'next');
-xnew = xuniq(index);
-ynew = yuniq(index);
+% Strip nans, and interpolate to make it complete
+knownIndices = ~isnan(rawLat);
+nanIndices   = isnan(rawLat);
+Lat = rawLat;
+Lon = rawLon;
+Lat(nanIndices) = interp1(rawDN(knownIndices), rawLat(knownIndices), rawDN(nanIndices), 'linear');
+Lon(nanIndices) = interp1(rawDN(knownIndices), rawLon(knownIndices), rawDN(nanIndices), 'linear');
 
-correctedDN = (dn(2:end)+dn(1:end-1))/2;
-correcttime = datetime(correctedDN,'ConvertFrom','datenum');
-% correctedVX = vx(1:end-1);
-% correctedVY = vy(1:end-1);
-correctedLat = xnew(1:end-1);
-correctedLon = ynew(1:end-1);
-correctedGPS = [correctedLat,correctedLon];
 
-% currents = [correctedDN correctedVX correctedVY];
-% vemsvxall = interp1(correctedDN,correctedVX,vems.dn,'next');
-% vemsvyall = interp1(correctedDN,correctedVY,vems.dn,'next');
-detectionlat = interp1(correctedDN,correctedLat,vems.mooredDN);
-detectionlon = interp1(correctedDN,correctedLon,vems.mooredDN);
-%
-% testlat = interp1(correctdn,correctlat,vems.dn);
-% testlon = interp1(correctdn,correctlon,vems.dn);
-%
-[~,TransIndex] = unique(vems.mooredDN);
-transmitters.DN = vems.mooredDN(TransIndex);
-transmitters.datetime = datetime(transmitters.DN,'ConvertFrom','datenum');
-% transmitters.tag = vems.mooredTag(TransIndex).';
-transmitters.id = vems.mooredID(TransIndex).';
-% transmitters.rec = vems.mooredRec(TransIndex).';
-% for k =1:length(transmitters.DN)
-%     if transmitters.rec(k) == 450282
-%        transmitters.UpDown{k,1} = 'UP';
-%     elseif transmitters.rec(k) ==450283
-%         transmitters.UpDown{k,1} = 'DOWN';
-%     end
-% end
-transmitters.gps_lat = detectionlat(TransIndex);
-transmitters.gps_lon = detectionlon(TransIndex);
-%
-% transmitters.badlat = testlat(TransIndex);
-% transmitters.badlon = testlon(TransIndex);
-%
-% transmitters.vx = interp1(correctedDN,correctedVX,vems.mooredDN,'next');
-% transmitters.vx = transmitters.vx(TransIndex);
-% transmitters.vy = interp1(correctedDN,correctedVY,vems.mooredDN,'next');
-% transmitters.vy = transmitters.vy(TransIndex);
-transmitters.density = transmitterdensity(TransIndex);
-transmitters.depth = transmitterdepth(TransIndex);
-transmitters.press = transmitterpress(TransIndex);
-transmitters.salt = transmittersalt(TransIndex);
-transmitters.temp = transmittertemp(TransIndex);
-spd = sndspd(transmittersalt,transmittertemp,transmitterdepth);
-transmitters.speedsound = spd(TransIndex);
-transmitters.latmean    = latmean;
-% transmitters.waterdepth = waterdepth(TransIndex);
+detectionDN = rawDN;
+detectionDT = datetime(detectionDN,'ConvertFrom','datenum');
+gliderLat = Lat(1:end-1);
+gliderLon = Lon(1:end-1);
+gliderGPS = [gliderLat,gliderLon];
+
+detectionlat = interp1(detectionDN,gliderLat,vems.dn);
+detectionlon = interp1(detectionDN,gliderLon,vems.dn);
+
+% I used to take out detections because both receivers caught it, but doing
+% to stop doing that for now.
+% [~,TransIndex] = unique(vems.dn);
+%I'm keeping this one memorialized so I remember how I did it.
+% detections.DN = vems.dn(TransIndex);
+
+detectionSalt = interp1(gliderDN,salt,detectionTime);
+detectionTemp = interp1(gliderDN,temperature,detectionTime);
+detectionPress = interp1(gliderDN,pressure,detectionTime);
+detectionDensity = interp1(gliderDN,density,detectionTime);
+detectionDepth = interp1(gliderDN,depth,detectionTime);
+
+
+
+detections.DN = vems.dn;
+detections.DT = datetime(detections.DN,'ConvertFrom','datenum');
+
+detections.receiver = vems.rec.';
+detections.tag = cell2mat(vems.tag.');
+detections.id = cellfun(@str2double,vems.id.');
+detections.gps_lat = detectionlat;
+detections.gps_lon = detectionlon;
+
+
+detections.density = detectionDensity;
+detections.depth = detectionDepth;
+detections.press = detectionPress;
+detections.salt = detectionSalt;
+detections.temp = detectionTemp;
+spd = sndspd(detectionSalt,detectionTemp,detectionDepth);
+detections.speedsound = spd;
+detections.latmean    = latmean;
+
+detectionReporting = [detections.DN, detections.id, detections.gps_lat, detections.gps_lon, detections.depth, detections.temp, detections.density]
 end
 
