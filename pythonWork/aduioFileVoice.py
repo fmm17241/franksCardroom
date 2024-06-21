5b11dfa465a9f51164339c2a1b63cf3a2c63f862
Frank McQuarrie <frankmac@uga.edu>
2024-06-21 11:08:43 -0400

    Audio processing
    processing

---
+++ pythonWork/aduioFileVoice.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 21 10:57:01 2024
+
+@author: fmm17241
+"""
+
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+import seaborn as sns
+
+from glob import glob
+
+import librosa
+import librosa.display
+import IPython.display as ipd
+
+from itertools import cycle
+
+sns.set_theme(style="white", palette=None)
+color_pal = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+color_cycle = cycle(plt.rcParams["axes.prop_cycle"].by_key()["color"])
+
+audio_files = glob(r"C:\Users\fmm17241\OneDrive - University of Georgia\data\audioFiles\*.wav")
+
+ipd.Audio(audio_files[0])
+
+
+march, sr = librosa.load(audio_files[1])
+print(f'y: {march[:10]}')
+print(f'shape march: {march.shape}')
+print(f'sr: {sr}')
+
+
+pd.Series(march).plot(figsize=(10, 5),
+                  lw=1,
+                  title='Raw Audio Example',
+                 color=color_pal[0])
+plt.show()
+
+
+# Trimming leading/lagging silence
+march_trimmed, _ = librosa.effects.trim(march, top_db=20)
+pd.Series(march_trimmed).plot(figsize=(10, 5),
+                  lw=1,
+                  title='Raw Audio Trimmed Example',
+                 color=color_pal[1])
+plt.show()
+
+
+
+D = librosa.stft(march)
+S_db = librosa.amplitude_to_db(np.abs(D), ref=np.max)
+S_db.shape
+
+
+fig, ax = plt.subplots(figsize=(10, 5))
+img = librosa.display.specshow(S_db,
+                              x_axis='time',
+                              y_axis='log',
+                              ax=ax)
+ax.set_title('Spectogram Example', fontsize=20)
+fig.colorbar(img, ax=ax, format=f'%0.2f')
+plt.show()
+
+
+S = librosa.feature.melspectrogram(y=march,
+                                   sr=sr,
+                                   n_mels=128 * 2,)
+S_db_mel = librosa.amplitude_to_db(S, ref=np.max)
+
+fig, ax = plt.subplots(figsize=(10, 5))
+# Plot the mel spectogram
+img = librosa.display.specshow(S_db_mel,
+                              x_axis='time',
+                              y_axis='log',
+                              ax=ax)
+ax.set_title('Mel Spectogram Example', fontsize=20)
+fig.colorbar(img, ax=ax, format=f'%0.2f')
+plt.show()
+
