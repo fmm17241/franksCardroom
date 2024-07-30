ee98390aea9769053642097dedc8062f0dace0ec
Frank McQuarrie <frankmac@uga.edu>
2024-07-30 14:03:19 -0400

    Update readDetectorData.m

--- readDetectorData.m
+++ readDetectorData.m
@@ -11,12 +11,45 @@ cd (fileDirectory)
 %Need to automate the date and time when files start and end
 %File has many characters; we need to convert the 10 characters after the
 %11th underscore into datetime
-
-counter      = 0;
 audioFiles   = dir('*.txt')
-startDT        =
-endDT         =
-totalSnaps   = 
+counter      = 0;
+startDT        = [];
+endDT         = [];
+totalSnaps   =  [];
+
+for k = 1:length(audioFiles)                %Loop to process each file
+   
+    file = audioFiles(k).name;
+    fid = fopen(file, 'rt');
+    
+    while true
+        line =fgetl(fid);
+        if ~ischar(line)                %If the lines empty, done, move to next file
+            fclose(fid);
+            break;
+        end
+        
+      if(contains(line,'STS'))     %If line contains STS, move to next line, they're empty.
+          continue;
+      end
+      Mydata=strsplit(line,',');   %split the data by the commas
+      receiver{end+1}=Mydata{1};   %Put data in previously made bins
+      number{end+1} = Mydata{2};
+      usedatetime{end+1}=Mydata{3};
+      tag{end+1} = Mydata{4};
+      id{end+1}= Mydata{5};
+      pound{end+1}= Mydata{6};
+    end   
+    counter = counter +1
+end
+receiver(1)=[];                    %Erase first value of all, its empty
+number(1)=[];
+usedatetime(1)=[];
+tag(1)=[];
+id(1)=[];
+pound(1)=[];
+
+
 
 %
 testing = readtable('sanctsound_audio_gr01_sanctsound_gr01_04_audio_SanctSound_GR01_04_5421_200129231235.wav.Table01.txt')
