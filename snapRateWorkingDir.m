18419618d2c30ff0cdee442b9ed127e8540d2d44
Frank McQuarrie <frankmac@uga.edu>
2024-09-16 15:52:10 -0400

    Update snapRateWorkingDir.m

--- snapRateWorkingDir.m
+++ snapRateWorkingDir.m
@@ -18,6 +18,7 @@ fileLocation = ([oneDrive,'\acousticAnalysis']);
 
 %%
 
+
 for K = 1:length(hourSnaps)
     clear snapTimeRange envFit
     snapTimeRange = [hourSnaps{K}.Time(1); hourSnaps{K}.Time(end)];
@@ -27,134 +28,6 @@ for K = 1:length(hourSnaps)
     envData{K} = receiverData{4}(envFit,:);
 
 end
-
-
-
-
-
-%
-for month = 1:length(envData)
-    for k = 1:2
-        windSpeedBins{k,month}(1,:) = envData{month}.windSpd < 2 & envData{month}.daytime == (k-1) ;
-        windSpeedBins{k,month}(2,:) = envData{month}.windSpd > 2 & envData{month}.windSpd < 4 & envData{month}.daytime == (k-1) ;
-        windSpeedBins{k,month}(3,:) = envData{month}.windSpd > 4 & envData{month}.windSpd < 6 & envData{month}.daytime == (k-1) ;
-        windSpeedBins{k,month}(4,:) = envData{month}.windSpd > 6 & envData{month}.windSpd < 8 & envData{month}.daytime == (k-1) ;
-        windSpeedBins{k,month}(5,:) = envData{month}.windSpd > 8 & envData{month}.windSpd < 10 & envData{month}.daytime == (k-1) ;
-        windSpeedBins{k,month}(6,:) = envData{month}.windSpd > 10 & envData{month}.windSpd < 12 & envData{month}.daytime == (k-1) ;
-        windSpeedBins{k,month}(7,:) = envData{month}.windSpd > 12 & envData{month}.windSpd < 14 & envData{month}.daytime == (k-1) ;
-        windSpeedBins{k,month}(8,:) = envData{month}.windSpd > 14 & envData{month}.daytime == (k-1) ;
-    end
-end
-
-%%
-for month = 1:length(envData)
-    for k = 1:2
-        for ii = 1:height(windSpeedBins{k,month})
-            % windSpeedScenario{k,month}= envData(windSpeedBins{k,month}(ii,:),:);
-            windSpeedScenario{k,month}= envData{1,month}(windSpeedBins{k,month}(ii,:),:);        
-
-            averageDets{month}(k,ii) = mean(windSpeedScenario{k,month}.HourlyDets,'omitnan');
-            noiseCompare{month}(k,ii) = mean(windSpeedScenario{k,month}.Noise,'omitnan');
-            wavesCompare{month}(k,ii) = mean(windSpeedScenario{k,month}.waveHeight,'omitnan');
-            tiltCompareWind{month}(k,ii) = mean(windSpeedScenario{k,month}.Tilt,'omitnan');
-            stratCompareWind{month}(k,ii) = mean(windSpeedScenario{k,month}.bulkThermalStrat,'omitnan')        
-        end
-    end
-end
-%%
-for month = 1:length(envData)
-    for k = 1:length(windSpeedBins)
-        for ii = 1:height(windSpeedBins{k,month})
-            binnedSnaps{k,month}{ii}   =  snaps{month}(windSpeedBins{k,month}(ii,:));
-        %
-            avgSnaps{k,month}(ii)  = mean(binnedSnaps{k,month}{ii},'omitNan')
-        end
-    end
-end
-
-
-% 
-% %Day Confidence Intervals
-% for k = 1:length(binnedSnaps)
-%     %Finding standard deviations/CIs of values
-%     SEM = std(binnedSnaps{1,k}(:),'omitnan')/sqrt(length(binnedSnaps{1,k}));  
-%     ts = tinv([0.025  0.975],length(binnedSnaps{1,k})-1);  
-%     CIdayNoise(k,:) = mean(binnedSnaps{1,k},'all','omitnan') + ts*SEM; 
-% 
-% end
-% 
-% 
-% 
-% %Day Confidence Intervals
-% for ii = 1:height(binnedSnaps)
-%     for k = 1:length(binnedSnaps)
-%     %Finding standard deviations/CIs of values
-%     SEM = std(binnedSnaps{ii,k}(:),'omitnan')/sqrt(length(binnedSnaps{ii,k}));  
-%     ts = tinv([0.025  0.975],length(binnedSnaps{ii,k})-1);  
-%     CIsnaps{ii}(k,:) = mean(binnedSnaps{i,k},'all','omitnan') + ts*SEM; 
-%     end
-% end
-
-
-
-X = 0:2:12
-
-
-figure()
-tiledlayout(2,1)
-ax1 = nexttile()
-scatter(X,avgSnaps{2},'filled','r')
-ylabel('Hourly Snaps')
-title('Shrimp Activity vs Wind, March','Day')
-ax2 = nexttile()
-scatter(X,avgSnaps{1},'filled','b')
-ylabel('Hourly Snaps')
-xlabel('Windspeed (m/s)')
-title('','Night')
-
-
-%%
-
-figure()
-scatter(X,avgSnaps{2},'filled','r')
-ylabel('Hourly Snaps')
-title('Shrimp Activity vs Wind, March','Day')
-hold on
-scatter(X,avgSnaps{1},'filled','b')
-ylabel('Hourly Snaps')
-xlabel('Windspeed (m/s)')
-
-
-
-figure()
-hold on
-% ciplot(CIsunsetNoise(:,1),CIsunsetNoise(:,2),1:5,'k')
-ciplot(CIsnaps{1}(:,1),CIsnaps{1}(:,2),1:7,'b')
-ciplot(CIsnaps{2}(:,1),CIsnaps{2}(:,2),1:7,'r')
-xlabel('Seasons, 2020')
-ylabel('Average Noise (mV)')
-title('Average Noise By Time of Day and Season','95% Conf. Interval, 69 kHz')
-legend('Night','Day')
-
-
-
-
-
-
-%mscohere()
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
