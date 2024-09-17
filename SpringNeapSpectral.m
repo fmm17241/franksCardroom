26ad0935a0d643135941b37c383e86c6466c8308
Frank McQuarrie <frankmac@uga.edu>
2024-09-16 20:46:36 -0400

    Update SpringNeapSpectral.m

--- SpringNeapSpectral.m
+++ SpringNeapSpectral.m
@@ -5,42 +5,65 @@
 %Running this after snapRateAnalyzer and snapRatePlotter, so I have nice
 %variables and structures to work with.
 % 
-% receiverData : cell structure of environmental and detection data.
+% envData : cell structure of environmental and detection data during the
+% times that we have analyzed hydrophones.
 % 
 % hourSnaps : hourly count of snaps at a certain threshold, 1000U if not
 % specified.
-%
+
+rawSignal = Power_spectra(hourSnaps{1}.SnapCount,1,1,0,3600,0)
+
+figure()
+plot(rawSignal.f*86400,rawSignal.psdw);
+title('Snaps','Raw Data')
+
+% Detrending the signal
+detrendedSignal = detrend(hourSnaps{1}.SnapCount,'constant')
+
+%Removing the mean from the signal:
+processedSignal = detrendedSignal - mean(detrendedSignal);
+
+%Find the frequency of the signal
+signalDetrended = Power_spectra(processedSignal,1,1,0,3600,0)
+
+% %This cuts the entire year+ dataset into 40 hour chunks
+% windowedSignal = buffer(processedSignal,40,20);  
+% paddedSignal = padarray(processedSignal,height(processedSignal)*2,'post');
 % 
 
 
+figure()
+plot(signalDetrended.f*86400,signalDetrended.psdw);
+title('Snaps','Detrended-Mean Data')
 
 
 
 
-% situationArray = [{weakWinter};{weakSummer};{strongWinter};{strongSummer}]
-% arrayDescrip = [{'Winter, Weak tides'};{'Summer, Weak tides'};{'Winter, Strong tides'};{'Summer, Strong tides'}];
 
 
-%%
-% Okay: FFT from strong and weak tides
-% for COUNT = 1:length(situationArray)
-%     detectionStruct{COUNT} = Power_spectra(situationArray{COUNT}.HourlyDets',1,1,0,3600,0)
-%     noiseStruct{COUNT} = Power_spectra(situationArray{COUNT}.Noise',1,1,0,3600,0)
-%     tideStruct{COUNT} = Power_spectra(situationArray{COUNT}.crossShore',1,1,0,3600,0)
-% end
+
+
+
+
+% freqDomainSignal = fft(processedSnaps);   % FFT to convert to frequency domain
 % 
+% % Optional: To visualize the frequency content
+% n = length(processedSnaps);                          % Number of points in the signal
+% frequencies = (0:n-1)*(1/(n*1));             % Frequency vector
+% amplitude = abs(freqDomainSignal/n);         % Amplitude of the frequencies
 % 
 % figure()
-% tiledlayout(length(detectionStruct),1)
-% for COUNT = 1:length(detectionStruct)
-%     nexttile()
-%     plot(detectionStruct{COUNT}.f*86400,detectionStruct{COUNT}.psdw)
-%     % xlim([0.7 12])
-%     % set(gca,'XScale','log')
-%     % set(gca,'YScale','log')
-%     title(sprintf('FFT Analysis: Detections, %s',arrayDescrip{COUNT}),'No Window, Detrended')
-%     ylim([0 20000000000])
-% end
+% plot(frequencies, amplitude);
+% xlabel('Frequency (Hz)');
+% ylabel('Amplitude');
+% title('Frequency Domain Signal');
+% 
+
+
+
+
+
+
 
 
 
