885a80bfd62eecfee7e3c2cd8621be145790f16e
Frank McQuarrie <frankmac@uga.edu>
2024-10-17 13:28:15 -0400

    Update franksFilterWonderland.m

--- franksFilterWonderland.m
+++ franksFilterWonderland.m
@@ -1,5 +1,5 @@
 %
-% I'm tired, but I have to try and test my filtering. I tried to use lowpass() but clearly something was wrong. 
+% I have to try and test my filtering. I tried to use lowpass() but clearly something was wrong. 
 % After applying a 40-hr lowpass filter, I could still find power and coherence at a diurnal and semidiurnal frequency.
 % Instead of lowpass(), I'm going to try creating and applying a butterworth filter.
 % I believe I've tried that before, but I'm flawed and need to figure it out.
@@ -31,29 +31,6 @@ times = surfaceData.time;
 % % Separate wind and wave data
 % load surfaceDataFall
 % times = surfaceData.time;
-
-
-
-% COHsnapWind = Coherence_whelch_overlap(snapRateHourly.SnapCount,surfaceData.waveHeight,3600,10,1,1,0)
-% COHnoiseWave = Coherence_whelch_overlap(envData.Noise,surfaceData.waveHeight,3600,10,1,1,0)
-% 
-% wavePower = Power_spectra(envData.waveHeight,4,0,0,3600,0)
-% snapPower = Power_spectra(snapRateHourly.SnapCount,4,0,0,3600,0)
-% 
-% figure()
-% loglog(wavePower.f*86400,wavePower.psdf)
-% title('Wave PSD')
-% 
-% 
-% figure()
-% loglog(COHnoiseWave.f*86400,COHnoiseWave.psdb)
-% title('Wave PSD')
-% 
-% figure()
-% semilogx(COHnoiseWave.f*86400,COHnoiseWave.coh)
-% title('Coherence NoiseWave')
-% 
-% close all
 
 %%
 % I want to smooth some of the snapt data. Technically snaps are events that are counted instantly
