309ca6ea2eb90e3d31174635d5d9bbd9f430c497
Frank McQuarrie <frankmac@uga.edu>
2024-10-31 22:32:02 -0400

    Update franksTalkingPoints.m

--- franksTalkingPoints.m
+++ franksTalkingPoints.m
@@ -31,6 +31,17 @@ snhaps = retime(snapRateHourly,'monthly','mean')
 
 
 
+decimatedData.Snaps = decimate(filteredData.Snaps,4);
+decimatedData.Noise = decimate(filteredData.Noise,4);
+decimatedData.Winds = decimate(filteredData.Winds,4);
+decimatedData.Waves = decimate(filteredData.Waves,4);
+decimatedData.Tides = decimate(filteredData.Tides,4);
+decimatedData.TidesAbsolute = decimate(filteredData.TidesAbsolute,4);
+decimatedData.WindDir = decimate(filteredData.WindDir,4);
+decimatedData.SBL = decimate(filteredData.SBL,4);
+decimatedData.SBLcapped = decimate(filteredData.SBLcapped,4);
+decimatedData.Detections = decimate(filteredData.Detections,4);
+decimatedData.SST = decimate(filteredData.SST,4);
 
 
 
@@ -105,20 +116,73 @@ linkaxes([ax1 ax2],'x')
 figure()
 fdg = tiledlayout(2,3)
 ax1 = nexttile([1,3])
-plot(times,envData.Noise)
+plot(times,envData.Noise,'LineWidth',1.5)
+title('HF Noise')
 
 ax2 = nexttile([1,3])
-plot(times,surfaceData.WSPD)
+plot(times,surfaceData.SBLcapped,'LineWidth',1.5)
+title('SBL')
 
 linkaxes([ax1 ax2],'x')
 
 
+figure()
+fdg = tiledlayout(2,3)
+ax1 = nexttile([1,3])
+plot(times(1429:1534),envData.Noise(1429:1534),'LineWidth',1.5)
+title('HF Noise')
+
+ax2 = nexttile([1,3])
+plot(times(1429:1534),surfaceData.SBLcapped(1429:1534),'LineWidth',1.5)
+title('SBL')
+
+X = 1:length(filteredData.Noise);
+
+figure()
+scatter(filteredData.SBL,filteredData.Noise,[],X);
+hold on
+scatter(filteredData.SBL(1429:1534),filteredData.Noise(1429:1534),[],X(1429:1534),'filled');
+xlabel('SBL')
+ylabel('HFnoise')
+
+[R P] = corrcoef(envData.Noise(1429:1534),surfaceData.SBLcapped(1429:1534))
+
+[R P] = corrcoef(filteredData.Noise(1429:1534),filteredData.SBLcapped(1429:1534))
+
+squrd = R(1,2)*R(1,2)
 
 
+%Yellow loop:
+% 1844 quiet
+%1892 loud
+
+%1792 loud
+% 1818 quiet
+
+%2097 quiet
+% 2141 loud
+
+%Green loop:
+% 1679 quiet
+%1609 loud
+%1566 mini quiet
+%  1534 loud
+% 1481 quiet
+% 1429 loud
+
+%Blue loop:
+% 601 loud
+% 524 quiet
+% 461 loud
 
 
+% 130 loud
+% 187 quiet
+% 259 loud
 
-
+%941 loud
+%871 quiet
+%810 loud
 
 
 
