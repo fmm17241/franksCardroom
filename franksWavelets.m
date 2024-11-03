509974867c0dd644ab5a70bf0c683ff5d21831e5
Frank McQuarrie <frankmac@uga.edu>
2024-11-03 12:47:11 -0500

    Update franksWavelets.m

--- franksWavelets.m
+++ franksWavelets.m
@@ -45,7 +45,7 @@ decimatedData.SST = decimate(filteredData.SST,4);
 
 
 % Assume `signal` is your time series data, and `Fs` is the sampling frequency
-[wt, f] = cwt(envData.crossShore, 3600);  % `Fs` should match the sampling rate of your recording
+[wt, f] = cwt(filteredData.SBLcapped, 1/3600);  % `Fs` should match the sampling rate of your recording
 % Plot the wavelet coefficients
 figure;
 pcolor(times, f, abs(wt));  % `t` is time, `f` is frequency vector from `cwt`
@@ -54,3 +54,43 @@ xlabel('Time (s)');
 ylabel('Frequency (Hz)');
 title('Wavelet Transform');
 colorbar;
+
+% Assume `signal` is your time series data, and `Fs` is the sampling frequency
+[wt, f] = cwt(filteredData.Snaps, 1/3600);  % `Fs` should match the sampling rate of your recording
+% Plot the wavelet coefficients
+figure;
+pcolor(times, f, abs(wt));  % `t` is time, `f` is frequency vector from `cwt`
+shading interp;
+xlabel('Time (s)');
+ylabel('Frequency (Hz)');
+title('Wavelet Transform');
+colorbar;
+
+
+
+%%
+signals = [snapRateHourly.SnapCount surfaceData.SBLcapped];
+
+numSignals = size(signals, 2);
+Fs = 1/3600; % Hourly sampling rate
+
+waveletCoefficients = cell(1, numSignals); % To store results
+frequencies = cell(1, numSignals);         % To store frequency scales
+
+for i = 1:numSignals
+    [wt, f] = cwt(signals(:, i), Fs);
+    % [wt, f] = cwt(signals(:, i), Fs, 'FrequencyLimits', [1e3, 20e3]);
+    waveletCoefficients{i} = wt;
+    frequencies{i} = f;
+end
+
+figure;
+for i = 1:numSignals
+    subplot(numSignals, 1, i);
+    pcolor(times, frequencies{i}, abs(waveletCoefficients{i}));  % Use pcolor to plot magnitude
+    shading interp;
+    xlabel('Time (s)');
+    ylabel('Frequency (Hz)');
+    title(['Signal ' num2str(i) ' Wavelet Transform']);
+    colorbar;
+end
