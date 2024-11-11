4b6c3709d4481222e1153ed7977b1b523f8a15e3
Frank McQuarrie <frankmac@uga.edu>
2024-11-11 14:32:33 -0500

    dvsf
    gfd

--- filterSnapData.m
+++ filterSnapData.m
@@ -18,6 +18,8 @@ filteredData.SBLcapped = filtfilt(Bsignal,Asignal,surfaceData.SBLcapped);
 filteredData.Detections = filtfilt(Bsignal,Asignal,envData.HourlyDets);
 filteredData.SST = filtfilt(Bsignal,Asignal,surfaceData.SST);
 filteredData.BottomTemp = filtfilt(Bsignal,Asignal,envData.Temp);
+filteredData.BulkStrat = filtfilt(Bsignal,Asignal,envData.bulkThermalStrat);
+
 
 % figure()
 % plot(snapRateHourly.Time,snapRateHourly.SnapCount,'LineWidth',1)
