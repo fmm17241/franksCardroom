672e770b4ae34961c744dece17f1a5f8a8a27082
Frank McQuarrie <frankmac@uga.edu>
2024-11-13 10:32:24 -0500

    Update remakingABfigures.m

--- remakingABfigures.m
+++ remakingABfigures.m
@@ -2,7 +2,8 @@
 
 %%
 %
-buildReceiverData   
+% buildReceiverData   
+load receiverDataONLYMOORINGS.mat
 
 
 for k = 1:length(receiverData)
@@ -23,15 +24,15 @@ plot(dailyAVG{2}.DT,dailyAVG{2}.HourlyDets)
 
 
 figure()
-plot(dailyAVG{1}.DT,dailyAVG{1}.Noise)
+plot(dailyAVG{2}.DT,dailyAVG{2}.Noise)
 yyaxis right
-plot(monthlyAVG{1}.DT,monthlyAVG{1}.HourlyDets)
+plot(monthlyAVG{2}.DT,monthlyAVG{2}.HourlyDets)
 
 [r,p] = corrcoef(dailyAVG{2}.Noise,dailyAVG{2}.HourlyDets)
 Rsqrd = r(1,2)*r(1,2)
 
 
-[r,p] = corrcoef(monthlyAVG{1}.Noise,monthlyAVG{1}.HourlyDets)
+[r,p] = corrcoef(monthlyAVG{2}.Noise,monthlyAVG{2}.HourlyDets)
 Rsqrd = r(1,2)*r(1,2)
 
 [r,p] = corrcoef(monthlyAVG{1}.bulkThermalStrat,monthlyAVG{1}.HourlyDets)
