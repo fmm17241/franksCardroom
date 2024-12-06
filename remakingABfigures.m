da572ea5b4de521f3c5322ce932549a1c898d9f6
Frank McQuarrie <frankmac@uga.edu>
2024-12-05 22:34:33 -0500

    Update remakingABfigures.m

--- remakingABfigures.m
+++ remakingABfigures.m
@@ -6,6 +6,33 @@ buildReceiverData
 % buildReceiverDataFISH
 % load receiverDataONLYMOORINGS.mat
 
+for k = 1:length(receiverData)
+    receiverData{k}.Temp = fillmissing(receiverData{k}.Temp,'linear')
+    receiverData{k}.HourlyDets = fillmissing(receiverData{k}.HourlyDets,'linear')
+    receiverData{k}.Noise = fillmissing(receiverData{k}.Noise,'linear')
+    receiverData{k}.windSpd = fillmissing(receiverData{k}.windSpd,'linear')
+    receiverData{k}.waveHeight = fillmissing(receiverData{k}.waveHeight,'linear')
+    receiverData{k}.bulkThermalStrat = fillmissing(receiverData{k}.bulkThermalStrat,'linear')
+        
+end
+
+
+clearvars only*
+noiseCondition = receiverData{5}.Noise <650;
+seasonCondition = receiverData{5}.Season == 5;
+
+selectedRows = noiseCondition & seasonCondition;
+
+onlymyRows = receiverData{5}(selectedRows,:);
+onlySSNRows = receiverData{5}(seasonCondition,:);
+
+
+
+
+[R,P] = corrcoef(receiverData{2}.windSpd,receiverData{2}.bulkThermalStrat)
+R(1,2)*R(1,2)
+
+
 %%
 % Collision analysis
 cd ('C:\Users\fmm17241\OneDrive - University of Georgia\data\collisionAnalysis')
