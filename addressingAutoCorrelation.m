23bf419a7b65d701550ae1bf9cd72040c3be0377
Frank McQuarrie <frankmac@uga.edu>
2025-01-03 11:35:47 -0500

    Update addressingAutoCorrelation.m

--- addressingAutoCorrelation.m
+++ addressingAutoCorrelation.m
@@ -27,52 +27,52 @@ times = surfaceData.time;
 
 
 %%
-% %%
-% %Auto correlation testing
-% 
-% filteredACF = autocorr(filteredData.Snaps,'NumLags',100)
-% rawACF = autocorr(receiverData{1}.Noise,'NumLags',100)
-% 
-% % Plot
-% lags = 0:100; % Include lag 0
-% plot(lags, rawACF, '-o', 'DisplayName', 'Raw Data');
-% hold on;
-% plot(lags, filteredACF, '-s', 'DisplayName', 'Filtered Data');
-% xlabel('Lag (hours)');
-% ylabel('Autocorrelation');
-% legend;
-% title('Autocorrelation Comparison');
-% hold off;
-% 
-% disp(['Lag 1 Raw ACF: ', num2str(rawACF(2)), ', Filtered ACF: ', num2str(filteredACF(2))]);
-% disp(['Lag 24 Raw ACF: ', num2str(rawACF(25)), ', Filtered ACF: ', num2str(filteredACF(25))]);
-% 
-% 
-% %%
-% % This tells me there's significant auto-correlation in our noise data
-% residuals = filteredData.Noise - mean(filteredData.Noise);
-% 
-% % Durbin-Watson statistic
-% numerator = sum(diff(residuals).^2);
-% denominator = sum(residuals.^2);
-% DW = numerator / denominator;
-% 
-% disp(['Durbin-Watson Statistic: ', num2str(DW)]);
-% %
-% 
-% laggedValue = lagmatrix(filteredData.Noise, 1); % Creates a 1-hour lag
-% model = fitlm([laggedValue, filteredData.Winds], filteredData.Noise);
-% figure()
-% plot(model)
-% 
-% 
-% crosscorr(receiverData{1}.windSpd, receiverData{1}.Noise, 'NumLags', 50);
-% 
-% laggedWind = lagmatrix(filteredData.Winds, [0 2]); % Includes current and lag-2 values
-% model = fitlm(laggedWind, filteredData.Noise);
-% 
-% residuals = model.Residuals.Raw;
-% autocorr(residuals, 'NumLags', 50);
+%%
+%Auto correlation testing
+
+filteredACF = autocorr(filteredData.Noise,'NumLags',100)
+rawACF = autocorr(receiverData{1}.Noise,'NumLags',100)
+
+% Plot
+lags = 0:100; % Include lag 0
+plot(lags, rawACF, '-o', 'DisplayName', 'Raw Data');
+hold on;
+plot(lags, filteredACF, '-s', 'DisplayName', 'Filtered Data');
+xlabel('Lag (hours)');
+ylabel('Autocorrelation');
+legend;
+title('Autocorrelation Comparison','Raw and Lowpass-Filtered (40hr) HF Noise');
+hold off;
+
+disp(['Lag 1 Raw ACF: ', num2str(rawACF(2)), ', Filtered ACF: ', num2str(filteredACF(2))]);
+disp(['Lag 24 Raw ACF: ', num2str(rawACF(25)), ', Filtered ACF: ', num2str(filteredACF(25))]);
+
+
+%%
+% This tells me there's significant auto-correlation in our noise data
+residuals = filteredData.Noise - mean(filteredData.Noise);
+
+% Durbin-Watson statistic
+numerator = sum(diff(residuals).^2);
+denominator = sum(residuals.^2);
+DW = numerator / denominator;
+
+disp(['Durbin-Watson Statistic: ', num2str(DW)]);
+%
+
+laggedValue = lagmatrix(filteredData.Noise, 1); % Creates a 1-hour lag
+model = fitlm([laggedValue, filteredData.Winds], filteredData.Noise);
+figure()
+plot(model)
+
+figure()
+crosscorr(receiverData{1}.windSpd, receiverData{1}.Noise, 'NumLags', 50)
+
+laggedWind = lagmatrix(filteredData.Winds, [0 2]); % Includes current and lag-2 values
+model = fitlm(laggedWind, filteredData.Noise);
+
+residuals = model.Residuals.Raw;
+autocorr(residuals, 'NumLags', 50);
 
 %
 %Frank needs to use ARIMA model to reduce autocorrelation which persists, or GLS
@@ -98,10 +98,12 @@ residualsFilt = olsFiltModel.Residuals.Raw;
 
 % Check autocorrelation in residuals
 figure()
-autocorr(residualsRaw, 'NumLags', 50);
+autocorr(residualsRaw, 'NumLags', 100);
+title('Sample Autocorrelation Function','Raw, Wind speed and HF Noise')
+
 
 figure()
-autocorr(residualsFilt, 'NumLags', 50);
+autocorr(residualsFilt, 'NumLags', 100);
 
 
 
