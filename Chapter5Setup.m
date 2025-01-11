e79736ac5e7890c820a01aa000f54c5d3d446ce3
Frank McQuarrie <frankmac@uga.edu>
2025-01-11 17:08:49 -0500

    Create Chapter5Setup.m

---
+++ Chapter5Setup.m
@@ -0,0 +1,15 @@
+%%
+
+%Frank getting serious about last chapter. Needs extensive work but looking
+%to build off the 2021 WUWnet paper. I'm considering working in Python,
+%I'll build my environmental data here then use Python to plot/model.
+
+% Required steps:
+% 1. Collect/Create environmental data as a medium for acousticp ropagation
+%
+% 2. Separate the data into single glider yos (yoDefiner or something)
+%
+% 3. Convert the yo into a sound speed profile (SSP), and format it into a
+%       *.env file to feed into Bellhop.
+%
+%
