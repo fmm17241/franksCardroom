d82a3c1b9dd729fb862c8b1e525cc11b712c5c4a
Frank McQuarrie <frankmac@uga.edu>
2025-01-13 14:13:27 -0500

    Update Chapter5Setup.m

--- Chapter5Setup.m
+++ Chapter5Setup.m
@@ -6,12 +6,17 @@
 
 % Required steps:
 % 1. Collect/Create environmental data as a medium for acoustic propagation
-datadir = 'C:\Users\fmac4\OneDrive - University of Georgia\data\Chapter5Scenarios\environmentalData\AprilMay2020';
+datadir = 'C:\Users\fmm17241\OneDrive - University of Georgia\data\Chapter5Scenarios\environmentalData\AprilMay2020';
 cd (datadir)
 load angusdbdAprilMay
 load angusebdAprilMay
 [matstruct,dn,z,temp] = Bindata(fstruct,sstruct);
 
+
+%Processs raw .vem files, the outputs of our sensors
+cd ([oneDrive,'Glider\Data\Vemco\SpringFallDets'])
+
+vems = vemProcess(pwd);
 
 % 2. Separate the data into single glider yos (yoDefiner or something)
 [yoSSP,yotemps,yotimes,yodepths,yosalt,yospeed] = yoDefinerAuto(dn, depth, temperature, salt, speed)
