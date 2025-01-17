c5bccee322c715f00822cab579db06ead58afacd
Frank McQuarrie <frankmac@uga.edu>
2025-01-17 16:16:59 -0500

    Update franksTalkingPoints.m

--- franksTalkingPoints.m
+++ franksTalkingPoints.m
@@ -1205,68 +1205,64 @@ hleg = legend('SBL','Snaps');
 htitle = get(hleg,'Title');
 set(htitle,'String','Raw Data')
 
+%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+
+load angusdbdAprilMay
+load angusebdAprilMay
+
+% Bindata for different missions
+[matstruct,dn,z,temp] = Bindata(fstruct,sstruct);
+
+dt = datetime(dn,'convertfrom','datenum')
+
+
+% test plot
+figure; 
+TTT = tiledlayout(5,3)
+ax1 = nexttile([2,3])
+yyaxis left
+h1=pcolor(dn,z,temp'); shading interp; colorbar; set(gca,'ydir','reverse'); 
+datetick('x','keeplimits');
+clim([20.5 22])
+xlim([dn(1,1) dn(1,170)])
+title('Water Column Profile: Temperature','Glider mission')
+ylabel('Z (m)')
+ylim([4 15])
+
+yyaxis right
+plot(datenum(times),surfaceData.WSPD,'LineWidth',4)
+ylabel('Windspeed (m/s)')
+
+
+ax2 = nexttile([1,3])
+yyaxis left 
+plot(times,envData.HourlyDets,'LineWidth',2)
+ylim([0 6])
+ylabel('Dets (\hr)')
+
+yyaxis right
+plot(times,envData.Noise,'LineWidth',2)
+ylim([500 780])
+ylabel('Noise (mV)')
+yline(650,'--','Challenging')
+title('','Detection Efficiency Versus Noise Interference')
+
+ax3 = nexttile([1,3])
+plot(times,surfaceData.SBLcapped,'r','LineWidth',3)
+yline(4.85,'--','Whitecaps  ')
+ylabel('SBL (dB)')
+title('','Surface Bubble Loss, Attenuation')
+
+ax4 = nexttile([1,3])
+plot(times,snapRateHourly.SnapCount,'k','LineWidth',2)
+title('','Benthic Activity')
+ylabel('Snaps (\hr)')
+
+linkaxes([ax2 ax3 ax4],'x')
 
 
 
-
-
-
-
-% load angusdbdAprilMay
-% load angusebdAprilMay
-% 
-% % Bindata for different missions
-% [matstruct,dn,z,temp] = Bindata(fstruct,sstruct);
-% 
-% dt = datetime(dn,'convertfrom','datenum')
-% 
-% 
-% % test plot
-% figure; 
-% TTT = tiledlayout(5,3)
-% ax1 = nexttile([2,3])
-% yyaxis left
-% h1=pcolor(dn,z,temp'); shading interp; colorbar; set(gca,'ydir','reverse'); 
-% datetick('x','keeplimits');
-% clim([20.5 22])
-% xlim([dn(1,1) dn(1,170)])
-% title('Water Column Profile: Temperature','Glider mission')
-% ylabel('Z (m)')
-% ylim([4 15])
-% 
-% yyaxis right
-% plot(datenum(times),surfaceData.WSPD,'LineWidth',4)
-% ylabel('Windspeed (m/s)')
-% 
-% 
-% ax2 = nexttile([1,3])
-% yyaxis left 
-% plot(times,envData.HourlyDets,'LineWidth',2)
-% ylim([0 6])
-% ylabel('Dets (\hr)')
-% 
-% yyaxis right
-% plot(times,envData.Noise,'LineWidth',2)
-% ylim([500 780])
-% ylabel('Noise (mV)')
-% yline(650,'--','Challenging')
-% title('','Detection Efficiency Versus Noise Interference')
-% 
-% ax3 = nexttile([1,3])
-% plot(times,surfaceData.SBLcapped,'r','LineWidth',3)
-% yline(4.85,'--','Whitecaps  ')
-% ylabel('SBL (dB)')
-% title('','Surface Bubble Loss, Attenuation')
-% 
-% ax4 = nexttile([1,3])
-% plot(times,snapRateHourly.SnapCount,'k','LineWidth',2)
-% title('','Benthic Activity')
-% ylabel('Snaps (\hr)')
-% 
-% linkaxes([ax2 ax3 ax4],'x')
-% 
-% 
-% 
 % 
 % 
 % 
